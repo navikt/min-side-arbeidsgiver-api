4d935b5595e149f7dbbbb963e204df5404422865
Bendik Segrov Ibenholt <bendik.ibenholt@bekk.no>
2019-03-08 13:09:35 +0100

    bruker lombok getter og setter i organization class

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/models/Organization.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/models/Organization.java
@@ -2,63 +2,18 @@ package no.nav.tag.dittNavArbeidsgiver.models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public class Organization {
-    private String name;
-    private String type;
-    private String organizationNumber;
-    private String organizationForm;
-    private String status;
-
-    public Organization() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String Name) {
-        name = Name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String Type) {
-        type = Type;
-    }
-
-    public String getOrganizationNumber() {
-        return organizationNumber;
-    }
-
-    public void setOrganizationNumber(String OrganizationNumber) {
-        organizationNumber = OrganizationNumber;
-    }
-
-    public String getOrganizationForm() {
-        return organizationForm;
-    }
-
-    public void setOrganizationForm(String OrganizationForm) {
-        organizationForm = OrganizationForm;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String Status) {
-        status = Status;
-    }
-
-    @Override
-    public String toString(){
-        return "nam: "+name+", type:" +type;
-    }
-
+    private String Name;
+    private String Type;
+    private String OrganizationNumber;
+    private String OrganizationForm;
+    private String Status;
 }
