4f4e39dc23cbd59fccfbb578e8f1289defb1d040
Bendik Segrov Ibenholt <bendik.ibenholt@bekk.no>
2019-03-14 16:14:24 +0100

    bruker data istedenfor getter og setter

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/services/altinn/AltinnConfig.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/services/altinn/AltinnConfig.java
@@ -1,19 +1,15 @@
 package no.nav.tag.dittNavArbeidsgiver.services.altinn;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
 @Component
+@Data
 @ConfigurationProperties(prefix = "altinn")
 public class AltinnConfig {
-    private String altinnHeader;
+    @Value("${altinnHeader}") private String altinnHeader;
     private String altinnurl;
     private String APIGwHeader;
 
