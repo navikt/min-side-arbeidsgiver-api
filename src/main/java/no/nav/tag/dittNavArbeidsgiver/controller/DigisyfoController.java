50fc1a8141e2eb2ad3a3ad364fee5242d83eba5c
Bendik Segrov Ibenholt <bendik.ibenholt@bekk.no>
2019-03-21 13:48:32 +0100

    forsøker å sende en spørring til digisyfo

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/DigisyfoController.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/DigisyfoController.java
@@ -3,9 +3,18 @@ package no.nav.tag.dittNavArbeidsgiver.controller;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.security.oidc.api.Protected;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
+import no.nav.tag.dittNavArbeidsgiver.services.altinn.AltinnException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Protected
 @Slf4j
@@ -22,13 +31,31 @@ public class DigisyfoController {
 
         @GetMapping(value = "/api/narmesteleder")
         public String sjekkNarmestelederTilgang() {
-            log.debug("===========Sjekknærmesteledertilgang===========");
+            RestTemplate restTemplate =new RestTemplate();
+            log.info("===========Sjekknærmesteledertilgang===========");
             for (String issuer : requestContextHolder.getOIDCValidationContext().getIssuers()){
-                log.debug("issuer: " + issuer);
-                log.debug("issuer: " + requestContextHolder.getOIDCValidationContext().getClaims(issuer));
+                log.info("issuer: " + issuer);
+                log.info("issuer: " + requestContextHolder.getOIDCValidationContext().getClaims(issuer));
             }
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Bearer ", requestContextHolder.getOIDCValidationContext().getToken("selvbetjening").getIdToken());
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            String url = "https://syfoarbeidsgivertilgang.nais.preprod.local/api/06025800174";
 
-            return "ok";
+            try {
+                ResponseEntity<String> respons = restTemplate.exchange(url,
+                        HttpMethod.GET, entity, String.class);
+                return respons.getBody();
+
+            } catch (RestClientException exception) {
+                log.error(" Ddigisyfo Exception: ", exception);
+                throw new AltinnException("Feil fra Altinn", exception);
+            }
+            // rest kall til syfo med header Bearer blabla  context.getToken("selvbetjening").getIdToken()
+
+            // restteplate legg til headers med "Bearer ..."
+
+           // return "ok";
         }
 
     }
