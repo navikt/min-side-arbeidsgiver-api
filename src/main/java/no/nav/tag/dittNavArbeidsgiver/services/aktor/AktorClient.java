3a643f863b449e43b01d879448157b391900db55
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-04-03 17:05:13 +0200

    TAG-410 Forslag til forbedring av AktorClient

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/services/aktor/AktorClient.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/services/aktor/AktorClient.java
@@ -9,17 +9,15 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
 @Component
 public class AktorClient {
 
-    @Value("${aktorregister.aktorUrl}")private String aktorUrl;
+    @Value("${aktorregister.aktorUrl}") private String aktorUrl;
     private final RestTemplate restTemplate;
     private final STSClient stsClient;
-    private String errorMessage;
 
     AktorClient(RestTemplate restTemplate,STSClient stsClient){
         this.restTemplate = restTemplate;
@@ -27,24 +25,22 @@ public class AktorClient {
     }
 
     public String getAktorId(String fnr){
+        try {
+            ResponseEntity<AktorResponse> response = utforKallTilAktorregister(fnr);
+            return response.getBody().get(fnr).identer.get(0).ident;
+        } catch(HttpClientErrorException e) {
+            throw new AktorException("Feil ved oppslag i aktørtjenesten", e);
+        }
+    }
+
+    private ResponseEntity<AktorResponse> utforKallTilAktorregister(String fnr) {
         String uriString = UriComponentsBuilder.fromHttpUrl(aktorUrl)
                 .queryParam("identgruppe","AktoerId")
                 .queryParam("gjeldende","true")
                 .toUriString();
         HttpEntity<String> entity = getRequestEntity(fnr);
-        try {
-            ResponseEntity<AktorResponse> response = restTemplate.exchange(uriString, HttpMethod.GET, entity, AktorResponse.class);
-            if(checkIfRequestWentWell(response) && checkIfAktorWasFound(fnr,response))
-                return (Objects.requireNonNull(response.getBody()).get(fnr).identer.get(0).ident);
-            else {
-                log.error(errorMessage);
-                throw new RuntimeException(errorMessage);
-            }
-        }
-        catch(HttpClientErrorException e){
-            log.error("Feil ved oppslag i aktørtjenesten", e);
-            throw new RuntimeException(e);
-        }
+
+        return restTemplate.exchange(uriString, HttpMethod.GET, entity, AktorResponse.class);
     }
 
     private HttpEntity<String> getRequestEntity(String fnr) {
@@ -56,31 +52,5 @@ public class AktorClient {
         headers.set("Nav-Consumer-Id", appName);
         headers.set("Nav-Personidenter", fnr);
         return new HttpEntity<>(headers);
-
-    }
-
-    private Boolean checkIfRequestWentWell(ResponseEntity<AktorResponse> response){
-        if (response.getStatusCode() != HttpStatus.OK) {
-            String message = "Kall mot aktørregister feiler med HTTP-" + response.getStatusCode();
-            log.error(message);
-            return false;
-        }
-        return true;
-    }
-
-    private Boolean checkIfAktorWasFound(String fnr, ResponseEntity<AktorResponse> response){
-        if (response.getBody() != null) {
-                if(response.getBody().get(fnr) == null) {
-                    errorMessage = "feilmelding på aktør: Fant ikke ident i respons fra aktørregister";
-                    return false;
-                }
-                if (response.getBody().get(fnr).feilmelding != null) {
-                    errorMessage = "feilmelding på aktør: " + response.getBody().get(fnr).feilmelding;
-                    return false;
-                }
-            return true;
-        }
-        errorMessage = "feil i forsøk på å hente aktør, tom mapping fra respons ";
-        return false;
     }
 }
