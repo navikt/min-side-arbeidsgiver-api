faa4c72cb613ac1c9429c5647404cabd9a6c21d0
bendiksi <benibe88@gmail.com>
2019-06-21 16:27:02 +0200

    endret digisyfoApi til å kun sende en boolean når man sjekker tilgang til digisyfo

---
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/models/DigisyfoNarmesteLederRespons.java
@@ -0,0 +1,11 @@
+package no.nav.tag.dittNavArbeidsgiver.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class DigisyfoNarmesteLederRespons {
+    NarmesteLeder [] narmesteLedere;
+    String[] humanResources;
+}
---
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/models/NarmesteLedertilgang.java
@@ -0,0 +1,5 @@
+package no.nav.tag.dittNavArbeidsgiver.models;
+
+public class NarmesteLedertilgang {
+    public Boolean tilgang;
+}
