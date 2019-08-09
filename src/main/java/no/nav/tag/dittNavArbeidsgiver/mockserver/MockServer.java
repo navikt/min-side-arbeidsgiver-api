36109f57fd067d2fe0aa25597b0cc545f013a7b5
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-08-09 13:19:31 +0200

    Flytter mockserver til egen pakke

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/mockserver/MockServer.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/mockserver/MockServer.java
@@ -1,4 +1,4 @@
-package no.nav.tag.dittNavArbeidsgiver.services.altinn;
+package no.nav.tag.dittNavArbeidsgiver.mockserver;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
