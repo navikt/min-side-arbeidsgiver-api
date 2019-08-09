7504c5bbed68105ad948d5ceb421b1fc8f0588dd
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-08-09 13:13:34 +0200

    Forenkler mock-oppsettet og fjerner noe unødvendig kode

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/services/altinn/MockServer.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/services/altinn/MockServer.java
@@ -4,7 +4,6 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
-import com.github.tomakehurst.wiremock.standalone.WireMockServerRunner;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -24,14 +23,11 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Component
 public class MockServer {
 
-    private AltinnConfig altinnConfig;
-
     @SneakyThrows
     @Autowired
     MockServer(
             @Value("${altinn.altinnUrl}") String altinnUrl,
             @Value("${mock.port}") int port,
-            AltinnConfig altinnConfig,
             @Value("${sts.stsUrl}") String stsUrl,
             @Value("${aad.aadAccessTokenURL}") String aadUrl,
             @Value("${aktorregister.aktorUrl}") String aktorUrl,
@@ -41,8 +37,7 @@ public class MockServer {
     ) {
         log.info("starter mockserveren");
 
-        this.altinnConfig = altinnConfig;
-        WireMockServer server = new WireMockServer(new WireMockConfiguration().port(port).extensions(new ResponseTemplateTransformer(false)));
+        WireMockServer server = new WireMockServer(new WireMockConfiguration().port(port).extensions(new ResponseTemplateTransformer(true)));
         String altinnPath = new URL(altinnUrl).getPath();
         String stsPath = new URL(stsUrl).getPath();
         String aadPath = new URL(aadUrl).getPath();
@@ -50,23 +45,20 @@ public class MockServer {
         String sykemeldtePath = new URL(sykemeldteUrl).getPath();
         String syfoOppgavePath = new URL(syfoOpggaveUrl).getPath();
         String syfoNarmesteLederPath = new URL(digisyfoUrl).getPath();
-        mockOrganisasjoner(altinnConfig, server, altinnPath);
-        mockInvalidSSN(altinnConfig, server, altinnPath);
-        mockRoles(altinnConfig,server,altinnPath);
-        mockSTSResponse(server, stsPath);
-        mockAadResponse(server, aadPath);
-        mockAktorResponse(server, aktorPath);
-        mockSykemeldingerResponse(server, sykemeldtePath);
-        mockSyfoOppgaverResponse(server, syfoOppgavePath);
-        mockSyfoNarmesteLeder(server, syfoNarmesteLederPath);
+        mockOrganisasjoner(server, altinnPath);
+        mockInvalidSSN(server, altinnPath);
+        mockForPath(server, altinnPath + "/authorization/roles", "roles.json");
+        mockForPath(server, stsPath, "STStoken.json");
+        mockForPath(server, aadPath, "aadtoken.json");
+        mockForPath(server, aktorPath, "aktorer.json");
+        mockForPath(server, sykemeldtePath, "sykemeldinger.json");
+        mockForPath(server, syfoOppgavePath, "syfoOppgaver.json");
+        mockForPath(server, syfoNarmesteLederPath, "narmesteLeder.json");
         server.start();
     }
 
-    public static void mockOrganisasjoner(AltinnConfig altinnConfig, WireMockServer server, String altinnPath) {
+    private static void mockOrganisasjoner(WireMockServer server, String altinnPath) {
         server.stubFor(WireMock.get(WireMock.urlPathEqualTo(altinnPath + "/reportees/"))
-                .withHeader("X-NAV-APIKEY", equalTo(altinnConfig.getAPIGwHeader()))
-                .withHeader("APIKEY", equalTo(altinnConfig.getAltinnHeader()))
-                .withQueryParam("ForceEIAuthentication", equalTo(""))
                 .withQueryParam("subject", equalTo("00000000000"))
                 .willReturn(WireMock.aResponse()
                         .withHeader("Content-Type", "application/json")
@@ -74,76 +66,19 @@ public class MockServer {
                 ));
     }
 
-    public static void mockInvalidSSN(AltinnConfig altinnConfig, WireMockServer server, String altinnPath) {
+    private static void mockInvalidSSN(WireMockServer server, String altinnPath) {
         server.stubFor(WireMock.get(WireMock.urlPathEqualTo(altinnPath + "/reportees/"))
-                .withHeader("X-NAV-APIKEY", equalTo(altinnConfig.getAPIGwHeader()))
-                .withHeader("APIKEY", equalTo(altinnConfig.getAltinnHeader()))
-                .withQueryParam("ForceEIAuthentication", equalTo(""))
-                .withQueryParam("subject", equalTo("04010100655"))
+                .withQueryParam("subject", WireMock.notMatching("00000000000"))
                 .willReturn(WireMock.aResponse().withStatusMessage("Invalid socialSecurityNumber").withStatus(400)
                         .withHeader("Content-Type", "application/octet-stream")
                 ));
     }
 
-    public static void mockRoles(AltinnConfig altinnConfig, WireMockServer server, String altinnPath) {
-        server.stubFor(WireMock.get(WireMock.urlPathEqualTo(altinnPath + "/authorization/roles"))
-                .withHeader("X-NAV-APIKEY", equalTo(altinnConfig.getAPIGwHeader()))
-                .withHeader("APIKEY", equalTo(altinnConfig.getAltinnHeader()))
-                .withQueryParam("ForceEIAuthentication", equalTo(""))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("roles.json"))
-                ));
-    }
-
-    public static void mockAktorResponse(WireMockServer server, String aktorURL) {
-        server.stubFor(WireMock.get(WireMock.urlPathEqualTo(aktorURL))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("aktorer.json"))
-                        .withTransformers(ResponseTemplateTransformer.NAME)
-                ));
-    }
-
-    public static void mockSTSResponse(WireMockServer server, String stsPath) {
-        server.stubFor(WireMock.get(WireMock.urlPathEqualTo(stsPath))
-                .withQueryParam("grant_type", equalTo("client_credentials"))
-                .withQueryParam("scope", equalTo("openid"))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("STStoken.json"))
-                ));
-    }
-
-    private void mockAadResponse(WireMockServer server, String aadPath) {
-        server.stubFor(WireMock.post(WireMock.urlPathEqualTo(aadPath))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("aadtoken.json"))
-                ));
-    }
-
-
-    public static void mockSykemeldingerResponse(WireMockServer server, String sykemeldtePath) {
-        server.stubFor(WireMock.get(WireMock.urlPathEqualTo(sykemeldtePath))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("sykemeldinger.json"))
-                ));
-    }
-
-    public static void mockSyfoOppgaverResponse(WireMockServer server, String syfoOppgavepath) {
-        server.stubFor(WireMock.get(WireMock.urlPathEqualTo(syfoOppgavepath))
-                .willReturn(WireMock.aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(hentStringFraFil("syfoOppgaver.json"))
-                ));
-    }
-    public static void mockSyfoNarmesteLeder(WireMockServer server, String syfoNarmesteLederPath){
-        server.stubFor(WireMock.get(WireMock.urlPathMatching(syfoNarmesteLederPath + ".*"))
+    private static void mockForPath(WireMockServer server, String path, String responseFile){
+        server.stubFor(WireMock.get(WireMock.urlPathMatching(path + ".*"))
             .willReturn(WireMock.aResponse()
                 .withHeader("Content-Type","application/json")
-                .withBody(hentStringFraFil("narmesteLeder.json"))
+                .withBody(hentStringFraFil(responseFile))
         ));
 
     }
