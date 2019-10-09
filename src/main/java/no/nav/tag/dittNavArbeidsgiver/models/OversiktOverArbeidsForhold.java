3c2c9f2877424df3319aacbfb9fe208d05b7dc8b
hilde.heggstad <hilde.heggstad@bekk.no>
2019-10-09 13:13:30 +0200

    lagd nørstede objekt for å bruke i oversikt over arbeidsforhold

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/models/OversiktOverArbeidsForhold.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/models/OversiktOverArbeidsForhold.java
@@ -10,8 +10,54 @@ import lombok.Data;
 @Data
 
 public class OversiktOverArbeidsForhold {
-    @JsonProperty("Name")
-    private String name;
+    @JsonProperty("antall")
+    private String antall;
+
+    public class ArbeidsForhold {
+        @JsonProperty("ansattFom")
+        private String ansattFom;
+        @JsonProperty("ansattTom")
+        private String ansattTom;
+
+        public class Arbeidsgiver {
+            @JsonProperty("type")
+            private String type;
+        }
+
+        public class Arbeidstaker {
+            @JsonProperty("type")
+            private String type;
+            @JsonProperty("aktoerId")
+            private String aktoerId;
+            @JsonProperty("offentligIdent")
+            private String offentligIdent;
+        }
+
+        @JsonProperty("arbeidsgiver")
+        private Arbeidsgiver arbeidsgiver;
+        @JsonProperty("arbeidstaker")
+        private Arbeidstaker arbeidstaker;
+
+
+
+
+
+
+
+
+
+
+
+        @JsonProperty("ParentOrganizationNumber")
+        private String parentOrganizationNumber;
+        @JsonProperty("OrganizationNumber")
+        private String organizationNumber;
+        @JsonProperty("OrganizationForm")
+        private String organizationForm;
+        @JsonProperty("Status")
+        private String status;
+    }
+
     @JsonProperty("Type")
     private String type;
     @JsonProperty("ParentOrganizationNumber")
