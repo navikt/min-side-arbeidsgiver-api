6d6819c6d30658b59bd13c9f373bb8bf9dbff34f
Torstein Gjengedal <torstein.gjengedal@nav.no>
2019-11-19 10:07:57 +0100

    Bruk RestTemplate som Spring Bean for å få metrikker

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/config/AppConfig.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
         return builder.build();
