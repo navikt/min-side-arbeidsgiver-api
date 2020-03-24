253663a96a1ac3f28d7548de66415490e5c84bdc
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2020-03-24 16:35:02 +0100

    Fungerte ikke lokalt

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/config/FeatureToggleConfig.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/config/FeatureToggleConfig.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-
+@Profile({"preprod", "prod"})
 @Configuration
 public class FeatureToggleConfig {
 
