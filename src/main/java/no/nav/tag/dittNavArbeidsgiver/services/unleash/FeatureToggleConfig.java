89ce12e7feec1e715ab098183462634b57afc56d
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-30 12:07:52 +0100

    endre unleashoppsett

---
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/services/unleash/FeatureToggleConfig.java
@@ -0,0 +1,43 @@
+package no.nav.tag.dittNavArbeidsgiver.services.unleash;
+
+
+import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.FakeUnleash;
+import no.finn.unleash.Unleash;
+import no.finn.unleash.util.UnleashConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class FeatureToggleConfig {
+
+    private static final String APP_NAME = "ditt-nav-arbeidsgiver-api";
+    private final static String UNLEASH_API_URL = "https://unleash.nais.adeo.no/api/";
+    @Bean
+    @Profile({"preprod", "prod"})
+    public Unleash initializeUnleash(
+                                     ByClusterStrategy byClusterStrategy)
+
+    {
+        UnleashConfig config = UnleashConfig.builder()
+                .appName(APP_NAME)
+                .instanceId(APP_NAME + "-" + System.getProperty("environment.name", "local"))
+                .unleashAPI(UNLEASH_API_URL)
+                .build();
+
+        return new DefaultUnleash(
+                config,
+                byClusterStrategy
+        );
+    }
+
+    @Profile("dev")
+    @Bean
+    public Unleash unleashMock() {
+        FakeUnleash fakeUnleash = new FakeUnleash();
+        fakeUnleash.enableAll();
+        return fakeUnleash;
+    }
+}
+
