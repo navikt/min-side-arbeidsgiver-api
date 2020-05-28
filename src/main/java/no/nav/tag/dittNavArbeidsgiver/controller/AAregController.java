b26a5d6271c153bba44f51a35dce0f6b5c5d9c1c
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-05-28 11:02:07 +0200

    bedre test og justeringer som burde fikse problemet

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/AAregController.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/AAregController.java
@@ -57,11 +57,11 @@ public class AAregController {
         log.info("MSA-AAREG controller hentArbeidsforhold orgnr: " + orgnr + " jurenhet: " + juridiskEnhetOrgnr );
         Timer kunArbeidstimer = MetricsFactory.createTimer("DittNavArbeidsgiverApi.kunArbeidsforhold").start();
         OversiktOverArbeidsForhold response = aAregServiceService.hentArbeidsforhold(orgnr,juridiskEnhetOrgnr,idToken);
-        if (response.getArbeidsforholdoversikter()==null) {
+        if (response.getArbeidsforholdoversikter()==null || response.getArbeidsforholdoversikter().length<=0) {
             log.info("MSA-AAREG controller hentArbeidsforhold fant ingen arbeidsforhold. Prøver å med overordnete enheter");
             response = finnOpplysningspliktigorg(orgnr, idToken);
         }
-        log.info("MSA-AAREG controller hentArbeidsforhold fant arbeidsforhold: " + response.getArbeidsforholdoversikter());
+        log.info("MSA-AAREG controller hentArbeidsforhold fant arbeidsforhold: {}", response.getArbeidsforholdoversikter());
         kunArbeidstimer.stop().report();
         OversiktOverArbeidsForhold arbeidsforholdMedNavn = settNavnPåArbeidsforhold(response);
         OversiktOverArbeidsForhold arbeidsforholdMedYrkesbeskrivelse = settYrkeskodebetydningPaAlleArbeidsforhold(arbeidsforholdMedNavn);
@@ -89,7 +89,7 @@ public class AAregController {
     public OversiktOverArbeidsForhold itererOverOrgtre(String orgnr, Organisasjoneledd orgledd, String idToken){
         OversiktOverArbeidsForhold result = aAregServiceService.hentArbeidsforhold(orgnr,orgledd.getOrganisasjonsnummer(),idToken);
         log.info("MSA-AAREG itererOverOrgtre orgnr: " +orgnr + "orgledd: "+ orgledd);
-        if(result.getArbeidsforholdoversikter()!=null){
+        if(result.getArbeidsforholdoversikter()!=null&&result.getArbeidsforholdoversikter().length>0){
             return result;
         }
         else if(orgledd.getInngaarIJuridiskEnheter()!=null){
