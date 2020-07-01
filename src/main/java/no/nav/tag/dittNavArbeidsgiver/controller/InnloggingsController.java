58c57467e67c78a08cdfc0598858d96707a847c5
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-07-01 10:48:21 +0200

    tagget med rest controller

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/InnloggingsController.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/InnloggingsController.java
@@ -4,8 +4,10 @@ import no.nav.security.oidc.api.Protected;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Protected
+@RestController
 public class InnloggingsController {
     @RequestMapping(value="/api/innlogget", method = RequestMethod.GET)
     @ResponseBody
