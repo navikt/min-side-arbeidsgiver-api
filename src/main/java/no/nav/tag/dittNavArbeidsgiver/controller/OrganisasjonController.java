5fc2c2f9593eeafbd4da8456a02eea0953ac4682
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-07-10 13:50:41 +0200

    fjernet kall i altinnservice som ikke er i bruk fra gammelt av

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/OrganisasjonController.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/OrganisasjonController.java
@@ -3,13 +3,11 @@ package no.nav.tag.dittNavArbeidsgiver.controller;
 import no.nav.security.oidc.api.Protected;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.tag.dittNavArbeidsgiver.models.Organisasjon;
-import no.nav.tag.dittNavArbeidsgiver.models.Role;
 import no.nav.tag.dittNavArbeidsgiver.services.altinn.AltinnService;
 import no.nav.tag.dittNavArbeidsgiver.utils.FnrExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
@@ -35,12 +33,6 @@ public class OrganisasjonController {
         String fnr = FnrExtractor.extract(requestContextHolder);
         List <Organisasjon> result = altinnService.hentOrganisasjoner(fnr);
         log.info("organisasjoner fra altinn:{}", result);
-        return ResponseEntity.ok(result);
-    }
-    @GetMapping(value ="/api/roller/{orgnr}")
-    public ResponseEntity<List<Role>> hentRoller(@PathVariable String orgnr){
-        String fnr = FnrExtractor.extract(requestContextHolder);
-        List<Role> result = altinnService.hentRoller(fnr, orgnr);
         return ResponseEntity.ok(result);
     }
 
