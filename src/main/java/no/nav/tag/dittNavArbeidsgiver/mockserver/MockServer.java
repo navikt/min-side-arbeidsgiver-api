2df31c9fc0e6e0166a3cb999aebeec6b0d254e52
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-10-08 08:12:40 +0200

    fikser wiremock slik at appen returenerer noe fra altinn endepunkt v lokal kjøring

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/mockserver/MockServer.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/mockserver/MockServer.java
@@ -5,6 +5,7 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
@@ -14,7 +15,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Profile({"dev"})
@@ -22,6 +27,9 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Component
 public class MockServer {
 
+    public static final String SERVICE_EDITION = "1";
+    public static final String SERVICE_CODE = "4936";
+    public static final String ALTINN_PROXY_PATH ="/altinn-rettigheter-proxy/v2/organisasjoner/*";
     @SneakyThrows
     @Autowired
     MockServer(
@@ -47,13 +55,14 @@ public class MockServer {
         String syfoOppgavePath = new URL(syfoOpggaveUrl).getPath();
         String syfoNarmesteLederPath = new URL(digisyfoUrl).getPath();
 
-
+        mockForPath(server, ALTINN_PROXY_PATH, "organisasjoner.json");
         mockForPath(server, stsPath, "STStoken.json");
         mockForPath(server, aadPath, "aadtoken.json");
         mockForPath(server, aktorPath, "aktorer.json");
         mockForPath(server, sykemeldtePath, "sykemeldinger.json");
         mockForPath(server, syfoOppgavePath, "syfoOppgaver.json");
         mockForPath(server, syfoNarmesteLederPath, "narmesteLeder.json");
+        mocktilgangTilSkjemForBedriftForAltinnProxy(server);
         server.start();
     }
 
@@ -62,6 +71,20 @@ public class MockServer {
                 .willReturn(WireMock.aResponse()
                         .withHeader("Content-Type","application/json")
                         .withBody(hentStringFraFil(responseFile))
+                ));
+    }
+    private static void mocktilgangTilSkjemForBedriftForAltinnProxy(
+            WireMockServer server
+    ) {
+        Map<String, StringValuePattern> parametre = new HashMap<>();
+        parametre.put("serviceCode", equalTo(SERVICE_CODE));
+        parametre.put("serviceEdition", equalTo(SERVICE_EDITION));
+
+        server.stubFor(WireMock.get(WireMock.urlPathMatching(ALTINN_PROXY_PATH + ".*"))
+                .withQueryParams(parametre)
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(hentStringFraFil("rettigheterTilSkjema.json"))
                 ));
     }
 
