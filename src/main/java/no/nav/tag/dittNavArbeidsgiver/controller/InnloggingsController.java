5fe992818e4bab5659e836bd1139e2b07bdc5be9
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-02-10 13:12:06 +0100

    forhindre cache av innloggingstilstand

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/InnloggingsController.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/controller/InnloggingsController.java
@@ -1,10 +1,13 @@
 package no.nav.tag.dittNavArbeidsgiver.controller;
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
+import org.springframework.http.CacheControl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 import static no.nav.tag.dittNavArbeidsgiver.utils.TokenUtils.ISSUER;
 import static no.nav.tag.dittNavArbeidsgiver.utils.TokenUtils.REQUIRED_LOGIN_LEVEL;
@@ -14,5 +17,7 @@ import static no.nav.tag.dittNavArbeidsgiver.utils.TokenUtils.REQUIRED_LOGIN_LEV
 public class InnloggingsController {
     @RequestMapping(value="/api/innlogget", method = RequestMethod.GET)
     @ResponseBody
-    public String erInnlogget(){return "ok";}
+    public String erInnlogget(){CacheControl cacheControl = CacheControl.noStore()
+            .mustRevalidate();
+    return "ok";}
 }
