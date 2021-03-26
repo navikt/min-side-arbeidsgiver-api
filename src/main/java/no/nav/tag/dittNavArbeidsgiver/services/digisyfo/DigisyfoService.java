89a380b38e62fe71321957eb3f9525447fefb7ac
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-03-26 10:19:16 +0100

    fjern unødvendige ting fra gcp miljø

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/services/digisyfo/DigisyfoService.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/services/digisyfo/DigisyfoService.java
@@ -2,6 +2,7 @@ package no.nav.tag.dittNavArbeidsgiver.services.digisyfo;
 
 import no.nav.tag.dittNavArbeidsgiver.models.DigisyfoNarmesteLederRespons;
 
+
 public interface DigisyfoService {
-    public DigisyfoNarmesteLederRespons getNarmesteledere(String fnr);
+    DigisyfoNarmesteLederRespons getNarmesteledere(String fnr);
 }
