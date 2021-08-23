0ac5f8bead95f550fc9f6b7254d4e44f8bd257b4
Ken Gullaksen <ken.gullaksen@nav.no>
2021-08-23 11:31:25 +0200

    log warning når vi får null response body fra digisyfo

--- src/main/java/no/nav/tag/dittNavArbeidsgiver/models/DigisyfoNarmesteLederRespons.java
+++ src/main/java/no/nav/tag/dittNavArbeidsgiver/models/DigisyfoNarmesteLederRespons.java
@@ -1,12 +1,16 @@
 package no.nav.tag.dittNavArbeidsgiver.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DigisyfoNarmesteLederRespons {
     List<Ansatt> ansatte;
 }
