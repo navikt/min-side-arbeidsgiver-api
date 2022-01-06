63c48cf71a0381e3856b9b76ec30d959b0e63d3c
Ken Gullaksen <ken.gullaksen@nav.no>
2022-01-06 10:39:22 +0100

    bruk turbofilter for å hindre stackoverflowerror (#220)
    * bruk turbofilter for å hindre stackoverflowerror
    
    * static import
    
    * erstatt forced exception med search logikk
    
    * erstatt forced exception med search logikk
    
    * Revert "erstatt forced exception med search logikk"
    
    This reverts commit 06e6b8ea50abd654e448eba0e0bb303520415331.
    
    * Revert "erstatt forced exception med search logikk"
    
    This reverts commit 276462599e7cb32fecf8ca208aab4a021724fc38.
    
    * catch exception fra new ThrowableProxy
    
    * tilbake til catch soe
    
    * deploy to dev
    
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

---
+++ src/main/java/no/nav/log/SOEPreventionFilter.java
@@ -0,0 +1,70 @@
+package no.nav.log;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ThrowableProxy;
+import ch.qos.logback.classic.turbo.TurboFilter;
+import ch.qos.logback.core.spi.FilterReply;
+import org.slf4j.Marker;
+
+import java.util.Arrays;
+
+import static no.nav.log.SOEPreventionFilter.ExceptionLoopDetector.hasLoop;
+
+/**
+ * Dette filteret er en hack for å unngå java.lang.StackOverflowError siden spring boot ikke støtter logback 1.3 P.T.
+ *
+ * ref:
+ * https://jira.qos.ch/browse/LOGBACK-1454
+ * https://github.com/spring-projects/spring-boot/issues/12649
+ *
+ * obs: Hele denne klassen kan fjernes når vi er over på en versjon av logback som takler exceptions med sirkusreferanser
+ */
+public class SOEPreventionFilter extends TurboFilter {
+
+    @Override
+    public FilterReply decide(
+            Marker marker,
+            Logger logger,
+            Level level,
+            String msg,
+            Object[] objects,
+            Throwable t
+    ) {
+        if (t != null) { // log.level("", ex)
+            if (hasLoop(t)) {
+                logger.log(marker, logger.getName(), Level.toLocationAwareLoggerInteger(level), msg, objects, new RuntimeException(t.getMessage()));
+                return FilterReply.DENY;
+            }
+        } else { // log.level("lorum {} ipsum {}", str1, obj2, ex)
+            Object lastArg = objects == null ? null : objects[objects.length - 1];
+            if (lastArg instanceof Throwable) {
+                if (hasLoop((Throwable) lastArg)) {
+                    Object[] copy = Arrays.copyOf(objects, objects.length);
+                    copy[objects.length - 1] = new RuntimeException(((Throwable) lastArg).getMessage());
+                    logger.log(marker, logger.getName(), Level.toLocationAwareLoggerInteger(level), msg, copy, t);
+                    return FilterReply.DENY;
+                }
+
+                return FilterReply.NEUTRAL;
+            }
+        }
+
+        return FilterReply.NEUTRAL;
+    }
+
+    static class ExceptionLoopDetector {
+
+        /**
+         * test om throwable lar seg proxye i logback
+         */
+        static boolean hasLoop(Throwable t) {
+            try {
+                new ThrowableProxy(t);
+                return false;
+            } catch (StackOverflowError e) {
+                return true;
+            }
+        }
+    }
+}
