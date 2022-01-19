cdcb1d724216583a0614201b37f12bf4ab7c74c0
Ken Gullaksen <ken.gullaksen@nav.no>
2022-01-19 13:33:21 +0100

    fiks tabell og index

--- src/main/resources/db/migration/V1__naermeste_leder.sql
+++ src/main/resources/db/migration/V1__naermeste_leder.sql
@@ -1,6 +1,8 @@
 create table naermeste_leder
 (
-    id                  uuid primary key,
+    id                  text primary key,
     naermeste_leder_fnr text not null
 );
+
+create index idx_naermeste_leder_naermeste_leder_fnr on naermeste_leder (naermeste_leder_fnr);
 
