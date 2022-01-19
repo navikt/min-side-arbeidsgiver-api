727dc1f803b4755b2a762b6c065880c542b96fc6
Ken Gullaksen <ken.gullaksen@nav.no>
2022-01-19 13:54:18 +0100

    fiks id til å være uuid

--- src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/NarmesteLederHendelse.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/NarmesteLederHendelse.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.UUID;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NarmesteLederHendelse {
-    public final String narmesteLederId;
+    public final UUID narmesteLederId;
     public final String narmesteLederFnr;
     public final String aktivTom;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public NarmesteLederHendelse(
-            @JsonProperty("narmesteLederId") String narmesteLederId,
+            @JsonProperty("narmesteLederId") UUID narmesteLederId,
             @JsonProperty("narmesteLederFnr") String narmesteLederFnr,
             @JsonProperty("aktivTom") String aktivTom
     ) {
