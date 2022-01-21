da20b0ea20159f150c9682cb04dce1f7174e09f3
Ken Gullaksen <ken.gullaksen@nav.no>
2022-01-21 09:57:59 +0100

    fjern countere på hartilgang (#229)

--- src/main/java/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.java
@@ -1,7 +1,5 @@
 package no.nav.arbeidsgiver.min_side.controller;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.arbeidsgiver.min_side.models.NarmesteLedertilgang;
 import no.nav.arbeidsgiver.min_side.services.digisyfo.NærmestelederRepository;
@@ -23,28 +21,20 @@ public class DigisyfoController {
 
     private final NærmestelederRepository nærmestelederRepository;
     private final TokenValidationContextHolder requestContextHolder;
-    private final Counter harTilgang;
-    private final Counter harIkkeTilgang;
 
     @Autowired
     public DigisyfoController(
-            MeterRegistry meterRegistry,
             NærmestelederRepository nærmestelederRepository,
             TokenValidationContextHolder requestContextHolder
     ) {
         this.nærmestelederRepository = nærmestelederRepository;
         this.requestContextHolder = requestContextHolder;
-        // TODO: ta bort disse coutnerne når vi ser at det går bra i prod
-        harTilgang = meterRegistry.counter("narmesteleder_tilgang", "hartilgang", "ja");
-        harIkkeTilgang = meterRegistry.counter("narmesteleder_tilgang", "hartilgang", "nei");
     }
 
     @GetMapping(value = "/api/narmesteleder")
     public NarmesteLedertilgang sjekkNarmestelederTilgang() {
-        boolean erNærmesteLeder;
         String fnr = FnrExtractor.extract(requestContextHolder);
-        erNærmesteLeder = nærmestelederRepository.erNærmesteLederForNoen(fnr);
-        (erNærmesteLeder ? harTilgang : harIkkeTilgang).increment();
+        boolean erNærmesteLeder = nærmestelederRepository.erNærmesteLederForNoen(fnr);
         return new NarmesteLedertilgang(erNærmesteLeder);
     }
 
