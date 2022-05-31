1f148b3d1eee4bad0e5ffa952167f43e7fde4377
Ken Gullaksen <ken.gullaksen@nav.no>
2022-05-31 08:56:33 +0200

    støtt både nytt og gammelt api
    Når det nye er på plass i frontend kan gammel api slettes/erstattes

--- src/main/java/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.java
@@ -6,7 +6,6 @@ import no.nav.arbeidsgiver.min_side.models.Organisasjon;
 import no.nav.arbeidsgiver.min_side.services.digisyfo.NærmestelederRepository;
 import no.nav.arbeidsgiver.min_side.services.ereg.EregService;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
