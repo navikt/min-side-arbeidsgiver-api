11e4d3261baf43630aeabe728458b2ff71580549
Ken Gullaksen <ken.gullaksen@nav.no>
2022-06-22 12:01:35 +0200

    legg til unit test på RefusjonStatusRepositoryImpl#statusoversikt

---
+++ src/test/groovy/no/nav/arbeidsgiver/min_side/services/tiltak/RefusjonStatusRepositoryImplTest.groovy
@@ -0,0 +1,30 @@
+package no.nav.arbeidsgiver.min_side.services.tiltak
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.jdbc.core.JdbcTemplate
+import spock.lang.Specification
+
+class RefusjonStatusRepositoryImplTest extends Specification {
+
+    def jdbcTemplate = Mock(JdbcTemplate)
+    def repo = new RefusjonStatusRepositoryImpl(new ObjectMapper(), jdbcTemplate)
+
+    def "henter liste av statusoversikt"() {
+        given:
+        def virksomhetsnumre = ["42","44"]
+        jdbcTemplate.queryForList(_ as String, virksomhetsnumre) >> [
+                ["virksomhetsnummer": "42", "status": "a", "count": 10],
+                ["virksomhetsnummer": "42", "status": "b", "count": 5],
+                ["virksomhetsnummer": "44", "status": "a", "count": 22],
+        ]
+
+        when:
+        def result = repo.statusoversikt(virksomhetsnumre)
+
+        then:
+        result.sort() == [
+                new RefusjonStatusRepository.Statusoversikt("42", ["a": 10, "b": 5]),
+                new RefusjonStatusRepository.Statusoversikt("44", ["a": 22]),
+        ].sort()
+    }
+}
