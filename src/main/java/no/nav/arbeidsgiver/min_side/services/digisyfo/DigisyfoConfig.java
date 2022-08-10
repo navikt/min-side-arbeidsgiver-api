a239043a8f3c7b9eb08102af90aef30fa96fcb87
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-08-10 13:46:59 +0200

    try logging exceptions

--- src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/DigisyfoConfig.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/DigisyfoConfig.java
@@ -1,5 +1,8 @@
 package no.nav.arbeidsgiver.min_side.services.digisyfo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.util.backoff.BackOff;
 import org.springframework.util.backoff.ExponentialBackOff;
 
 @Profile({"dev-gcp", "prod-gcp"})
@@ -31,9 +36,23 @@ public class DigisyfoConfig {
     ) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties()));
-        factory.setCommonErrorHandler(new DefaultErrorHandler(new ExponentialBackOff()));
+        factory.setCommonErrorHandler(new DefaultErrorHandlerWithLogging(new ExponentialBackOff()));
         factory.setBatchListener(true);
         return factory;
     }
 
+    @Slf4j
+    static class DefaultErrorHandlerWithLogging extends DefaultErrorHandler {
+        DefaultErrorHandlerWithLogging(BackOff backOff) {
+            super(backOff);
+        }
+
+        @Override
+        public void handleBatch(Exception thrownException, ConsumerRecords<?, ?> data, Consumer<?, ?> consumer, MessageListenerContainer container, Runnable invokeListener) {
+            if (thrownException != null) {
+                log.warn("exception while handling batch: {}", thrownException.getMessage(), thrownException);
+            }
+            super.handleBatch(thrownException, data, consumer, container, invokeListener);
+        }
+    }
 }
