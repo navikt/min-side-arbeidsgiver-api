b23309f75c816597d9c67b67ad25c4aa2fc0f188
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-08-11 11:52:43 +0200

    make component

--- src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/NærmesteLederKafkaConsumerImpl.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/NærmesteLederKafkaConsumerImpl.java
@@ -6,8 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
+@Profile({"dev-gcp", "prod-gcp"})
 public class NærmesteLederKafkaConsumerImpl {
     private final ObjectMapper objectMapper;
     private final NærmestelederRepository nærmestelederRepository;
