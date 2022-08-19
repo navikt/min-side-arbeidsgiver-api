a7e3ec19c6f8d7dc9c3c949c4b152a8d776f5a07
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2022-08-19 09:44:34 +0200

    Implementer endepunkt for virksomheter hvor man er nærmeste leder + antall sykmeldinger

--- src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/SykmeldingRepository.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/SykmeldingRepository.java
@@ -1,7 +1,6 @@
 package no.nav.arbeidsgiver.min_side.services.digisyfo;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 import java.util.Map;
