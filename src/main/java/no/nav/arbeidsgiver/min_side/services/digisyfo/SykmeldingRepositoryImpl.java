993529fe7552d599aebccb64dd9fcd15438fae1a
Ken Gullaksen <ken.gullaksen@nav.no>
2022-08-22 11:48:04 +0200

    wrap queryForStream in try with resources

--- src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/SykmeldingRepositoryImpl.java
+++ src/main/java/no/nav/arbeidsgiver/min_side/services/digisyfo/SykmeldingRepositoryImpl.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +21,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Profile({"dev-gcp", "prod-gcp"})
 @Repository
@@ -54,26 +54,27 @@ public class SykmeldingRepositoryImpl implements SykmeldingRepository {
 
     @Override
     public Map<String, Integer> oversiktSykmeldinger(String nærmestelederFnr) {
-        return jdbcTemplate.queryForStream(
-                        """
-                                with nl as (
-                                    select virksomhetsnummer, ansatt_fnr from naermeste_leder where naermeste_leder_fnr = ?
-                                )
-                                select s.virksomhetsnummer as virksomhetsnummer, count(*) as antall
-                                from sykmelding as s
-                                join nl on 
-                                nl.virksomhetsnummer = s.virksomhetsnummer and 
-                                nl.ansatt_fnr = s.ansatt_fnr
-                                group by s.virksomhetsnummer
-                            """,
-                        (PreparedStatement ps) ->
-                                ps.setString(1, nærmestelederFnr),
-                        (RowMapper<Pair<String, Integer>>) (ResultSet rs, int row) -> new ImmutablePair<>(
-                                rs.getString("virksomhetsnummer"),
-                                rs.getInt("antall")
-                        )
+        try (Stream<Pair<String, Integer>> stream = jdbcTemplate.queryForStream(
+                """
+                            with nl as (
+                                select virksomhetsnummer, ansatt_fnr from naermeste_leder where naermeste_leder_fnr = ?
+                            )
+                            select s.virksomhetsnummer as virksomhetsnummer, count(*) as antall
+                            from sykmelding as s
+                            join nl on
+                            nl.virksomhetsnummer = s.virksomhetsnummer and
+                            nl.ansatt_fnr = s.ansatt_fnr
+                            group by s.virksomhetsnummer
+                        """,
+                (PreparedStatement ps) ->
+                        ps.setString(1, nærmestelederFnr),
+                (ResultSet rs, int row) -> new ImmutablePair<>(
+                        rs.getString("virksomhetsnummer"),
+                        rs.getInt("antall")
                 )
-                .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+        )) {
+            return stream.collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+        }
     }
 
     /**
