9a209dd897304bb838147d6c7cbfdfdbdc0a1859
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-22 20:51:01 +0100

    konvert AppConfig to kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/config/AppConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/config/AppConfig.kt
@@ -1,65 +1,59 @@
-package no.nav.arbeidsgiver.min_side.config;
+package no.nav.arbeidsgiver.min_side.config
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.lang.invoke.MethodHandles;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.function.Predicate;
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.client.RestTemplateCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import java.lang.invoke.MethodHandles
+import java.util.*
 
 @Configuration
 @EnableScheduling
-public class AppConfig {
-
-    public static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    public static final String CALL_ID = "callId";
-
+class AppConfig {
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder.build()
 
     /**
      * propagerer callid fra MDC til request header
      */
     @Bean
-    public RestTemplateCustomizer callIdRestTemplateCustomizer() {
-        return restTemplate -> restTemplate.getInterceptors().add((request, body, execution) -> {
-            request.getHeaders().addIfAbsent(CALL_ID, MDC.get(CALL_ID));
-            return execution.execute(request, body);
-        });
+    fun callIdRestTemplateCustomizer(): RestTemplateCustomizer {
+        return RestTemplateCustomizer { restTemplate: RestTemplate ->
+            restTemplate.interceptors.add(
+                ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+                    request.headers.addIfAbsent(CALL_ID, MDC.get(CALL_ID))
+                    execution.execute(request, body!!)
+                })
+        }
     }
 
     /**
      * log basic info om request response via resttemplate
      */
     @Bean
-    public RestTemplateCustomizer loggingInterceptorCustomizer() {
-        return restTemplate -> restTemplate.getInterceptors().add((request, body, execution) -> {
-            log.info("RestTemplate.request: {} {}{}", request.getMethod(), request.getURI().getHost(), request.getURI().getPath());
-            ClientHttpResponse response = execution.execute(request, body);
-            log.info("RestTemplate.response: {} {}", response.getStatusCode(), response.getHeaders().getContentLength());
-            return response;
-        });
+    fun loggingInterceptorCustomizer() = RestTemplateCustomizer { restTemplate: RestTemplate ->
+        restTemplate.interceptors.add(
+            ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+                log.info("RestTemplate.request: {} {}{}", request.method, request.uri.host, request.uri.path)
+                val response = execution.execute(request, body!!)
+                log.info("RestTemplate.response: {} {}", response.statusCode, response.headers.contentLength)
+                response
+            })
     }
 
     /**
@@ -67,64 +61,66 @@ public class AppConfig {
      * propagerer også callid til response header
      */
     @Bean
-    public OncePerRequestFilter callIdTilMdcFilter() {
-        List<String> kjenteHeaderNavn = List.of(
-                "X-Request-ID",
-                "X-Correlation-ID",
-                CALL_ID,
-                "call-id",
-                "call_id",
-                "x_callId"
-        );
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(
-                    @NotNull HttpServletRequest request,
-                    @NotNull HttpServletResponse response,
-                    @NotNull FilterChain chain) throws ServletException, IOException {
+    fun callIdTilMdcFilter(): OncePerRequestFilter {
+        val kjenteHeaderNavn = listOf(
+            "X-Request-ID",
+            "X-Correlation-ID",
+            CALL_ID,
+            "call-id",
+            "call_id",
+            "x_callId"
+        )
+        return object : OncePerRequestFilter() {
+            @Throws(ServletException::class, IOException::class)
+            override fun doFilterInternal(
+                request: HttpServletRequest,
+                response: HttpServletResponse,
+                chain: FilterChain
+            ) {
                 try {
-                    String callId = kjenteHeaderNavn.stream()
-                            .map(request::getHeader)
-                            .filter(Objects::nonNull)
-                            .filter(Predicate.not(String::isBlank))
-                            .findFirst()
-                            .orElseGet(() -> UUID.randomUUID().toString());
-                    MDC.put(CALL_ID, callId);
-                    response.setHeader(CALL_ID, callId);
-                    chain.doFilter(request, response);
+                    val callId = kjenteHeaderNavn
+                        .map { request.getHeader(it) }
+                        .filter(Objects::nonNull)
+                        .firstOrNull(String::isNotBlank) ?: UUID.randomUUID().toString()
+                    MDC.put(CALL_ID, callId)
+                    response.setHeader(CALL_ID, callId)
+                    chain.doFilter(request, response)
                 } finally {
-                    MDC.remove(CALL_ID);
+                    MDC.remove(CALL_ID)
                 }
             }
-        };
+        }
     }
 
     /**
      * log basic info om request response på våre endepunkter
      */
     @Bean
-    public OncePerRequestFilter requestResponseLoggingFilter() {
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(
-                    @NotNull HttpServletRequest request,
-                    @NotNull HttpServletResponse response,
-                    @NotNull FilterChain chain) throws ServletException, IOException {
-                try {
-                    log.info("servlet.request {} {}", request.getMethod(), request.getRequestURI());
-                    chain.doFilter(request, response);
-                } finally {
-                    log.info(
-                            "servlet.response {} {} => {}",
-                            request.getMethod(), request.getRequestURI(), HttpStatus.resolve(response.getStatus())
-                    );
-                }
+    fun requestResponseLoggingFilter() = object : OncePerRequestFilter() {
+        @Throws(ServletException::class, IOException::class)
+        override fun doFilterInternal(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            chain: FilterChain
+        ) {
+            try {
+                log.info("servlet.request {} {}", request.method, request.requestURI)
+                chain.doFilter(request, response)
+            } finally {
+                log.info(
+                    "servlet.response {} {} => {}",
+                    request.method, request.requestURI, HttpStatus.resolve(response.status)
+                )
             }
+        }
 
-            @Override
-            protected boolean shouldNotFilter(@NotNull HttpServletRequest request) {
-                return request.getRequestURI().contains("internal/actuator");
-            }
-        };
+        override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+            return request.requestURI.contains("internal/actuator")
+        }
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+        const val CALL_ID = "callId"
     }
 }
