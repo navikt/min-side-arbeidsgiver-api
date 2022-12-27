0b3aeabbb4d6d0de6c0eba2e4619e82a072c3575
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-27 14:36:17 +0100

    konverter TokenSupportJwtConfig til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/TokenSupportJwtConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/TokenSupportJwtConfig.kt
@@ -1,19 +1,10 @@
-package no.nav.arbeidsgiver.min_side;
+package no.nav.arbeidsgiver.min_side
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Configuration
 
-@ConditionalOnProperty(
-        value = "tokensupport.enabled",
-        havingValue = "true",
-        matchIfMissing = true
-)
-@EnableJwtTokenValidation(ignore = {
-        "org.springdoc",
-        "org.springframework"
-})
+@ConditionalOnProperty(value = ["tokensupport.enabled"], havingValue = "true", matchIfMissing = true)
+@EnableJwtTokenValidation(ignore = ["org.springdoc", "org.springframework"])
 @Configuration
-public class TokenSupportJwtConfig {
-
-}
+class TokenSupportJwtConfig 
