584266cce7b8f19a1190120e79862678aac1eb9e
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-27 13:16:49 +0100

    konverter AltinnCacheConfig til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/services/altinn/AltinnCacheConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/services/altinn/AltinnCacheConfig.kt
@@ -1,33 +1,39 @@
-package no.nav.arbeidsgiver.min_side.services.altinn;
+package no.nav.arbeidsgiver.min_side.services.altinn
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.TimeUnit;
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.caffeine.CaffeineCache
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
-public class AltinnCacheConfig {
-
-    final static String ALTINN_CACHE = "altinn_cache";
-    final static String ALTINN_TJENESTE_CACHE = "altinn_tjeneste_cache";
+class AltinnCacheConfig {
     @Bean
-    public CaffeineCache altinnCache(){
-        return new CaffeineCache(ALTINN_CACHE,
-                Caffeine.newBuilder()
-                        .maximumSize(10000)
-                        .expireAfterWrite(30, TimeUnit.MINUTES)
-                        .recordStats()
-                        .build());
+    fun altinnCache(): CaffeineCache {
+        return CaffeineCache(
+            ALTINN_CACHE,
+            Caffeine.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .recordStats()
+                .build()
+        )
     }
+
     @Bean
-    public CaffeineCache altinnTjenesteCache(){
-        return new CaffeineCache(ALTINN_TJENESTE_CACHE,
-                Caffeine.newBuilder()
-                        .maximumSize(10000)
-                        .expireAfterWrite(30, TimeUnit.MINUTES)
-                        .recordStats()
-                        .build());
+    fun altinnTjenesteCache(): CaffeineCache {
+        return CaffeineCache(
+            ALTINN_TJENESTE_CACHE,
+            Caffeine.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .recordStats()
+                .build()
+        )
+    }
+
+    companion object {
+        const val ALTINN_CACHE = "altinn_cache"
+        const val ALTINN_TJENESTE_CACHE = "altinn_tjeneste_cache"
     }
 }
