9d924844ca59b587079dfb9f1610dba3f09dc814
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-27 13:15:46 +0100

    fjern ubrukt api endepunkt

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/DigisyfoController.kt
@@ -2,16 +2,9 @@ package no.nav.arbeidsgiver.min_side.controller
 
 import no.nav.arbeidsgiver.min_side.models.Organisasjon
 import no.nav.arbeidsgiver.min_side.services.digisyfo.DigisyfoService
-import no.nav.arbeidsgiver.min_side.services.digisyfo.NærmestelederRepository
-import no.nav.arbeidsgiver.min_side.services.digisyfo.SykmeldingRepository
-import no.nav.arbeidsgiver.min_side.services.ereg.EregService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
-import java.util.function.Predicate
-import java.util.stream.Collectors
-import java.util.stream.Stream
 
 @ProtectedWithClaims(
     issuer = AuthenticatedUserHolder.TOKENX,
@@ -20,9 +13,6 @@ import java.util.stream.Stream
 @RestController
 class DigisyfoController(
     private val digisyfoService: DigisyfoService,
-    private val nærmestelederRepository: NærmestelederRepository,
-    private val sykmeldingRepository: SykmeldingRepository,
-    private val eregService: EregService,
     private val authenticatedUserHolder: AuthenticatedUserHolder
 ) {
 
@@ -32,46 +22,6 @@ class DigisyfoController(
     )
 
     @GetMapping("/api/narmesteleder/virksomheter-v3")
-    fun hentVirksomheter(): Collection<VirksomhetOgAntallSykmeldte> {
-        val fnr = authenticatedUserHolder.fnr
-        return digisyfoService.hentVirksomheterOgSykmeldte(fnr)
-    }
-
-    data class DigisyfoOrganisasjonBakoverkompatibel(
-        val organisasjon: Organisasjon? = null,
-        val antallSykmeldinger: Int = 0,
-    )
-
-    @GetMapping("/api/narmesteleder/virksomheter-v2")
-    fun hentVirksomheterBakoverkompatibel(): List<DigisyfoOrganisasjonBakoverkompatibel> {
-        val fnr = authenticatedUserHolder.fnr
-        val aktiveSykmeldingerOversikt = sykmeldingRepository.oversiktSykmeldinger(fnr)
-        val harAktiveSykmeldinger = Predicate { virksomhetsnummer: String? ->
-            aktiveSykmeldingerOversikt.getOrDefault(
-                virksomhetsnummer,
-                0
-            ) > 0
-        }
-        return nærmestelederRepository.virksomheterSomNærmesteLeder(fnr)
-            .stream()
-            .filter(harAktiveSykmeldinger)
-            .flatMap { virksomhetsnummer: String? -> hentUnderenhetOgOverenhet(virksomhetsnummer) }
-            .filter { obj: Organisasjon? -> Objects.nonNull(obj) }
-            .map { org: Organisasjon? ->
-                DigisyfoOrganisasjonBakoverkompatibel(
-                    org,
-                    aktiveSykmeldingerOversikt.getOrDefault(org!!.organizationNumber, 0)
-                )
-            }
-            .collect(Collectors.toList())
-    }
-
-    fun hentUnderenhetOgOverenhet(virksomhetsnummer: String?): Stream<Organisasjon?> {
-        val underenhet = eregService.hentUnderenhet(virksomhetsnummer)
-        var overenhet: Organisasjon? = null
-        if (underenhet != null) {
-            overenhet = eregService.hentOverenhet(underenhet.parentOrganizationNumber)
-        }
-        return Stream.of(underenhet, overenhet)
-    }
+    fun hentVirksomheter(): Collection<VirksomhetOgAntallSykmeldte> =
+        digisyfoService.hentVirksomheterOgSykmeldte(authenticatedUserHolder.fnr)
 }
--- src/test/kotlin/no/nav/arbeidsgiver/min_side/controller/DigisyfoControllerTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/controller/DigisyfoControllerTest.kt
@@ -2,14 +2,7 @@ package no.nav.arbeidsgiver.min_side.controller
 
 import no.nav.arbeidsgiver.min_side.models.Organisasjon
 import no.nav.arbeidsgiver.min_side.services.digisyfo.DigisyfoService
-import no.nav.arbeidsgiver.min_side.services.digisyfo.NærmestelederRepository
-import no.nav.arbeidsgiver.min_side.services.digisyfo.SykmeldingRepository
-import no.nav.arbeidsgiver.min_side.services.ereg.EregService
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentCaptor
-import org.mockito.Captor
 import org.mockito.Mockito.`when`
 import org.skyscreamer.jsonassert.JSONAssert
 import org.springframework.beans.factory.annotation.Autowired
@@ -33,75 +26,11 @@ class DigisyfoControllerTest {
     lateinit var authenticatedUserHolder: AuthenticatedUserHolder
 
     @MockBean
-    lateinit var nærmestelederRepository: NærmestelederRepository
-
-    @MockBean
-    lateinit var sykmeldingRepository: SykmeldingRepository
-
-    @MockBean
-    lateinit var eregService: EregService
-
-    @MockBean
     lateinit var digisyfoService: DigisyfoService
-
-    @Captor
-    lateinit var orgnrCaptor: ArgumentCaptor<String>
-
-    val enhetsregisteret = mapOf(
-        "1" to mkOverenhet("1"),
-        "10" to mkUnderenhet("10", "1"),
-        "2" to mkOverenhet("2"),
-        "20" to mkUnderenhet("20", "2"),
-        "3" to mkOverenhet("3"),
-        "30" to mkUnderenhet("30", "3"),
-    )
-
-    @BeforeEach
-    fun setUp() {
-        `when`(authenticatedUserHolder.fnr).thenReturn("42")
-
-        `when`(
-            eregService.hentOverenhet(orgnrCaptor.capture())
-        ).thenAnswer {
-            enhetsregisteret[orgnrCaptor.value]
-        }
-
-        `when`(
-            eregService.hentUnderenhet(orgnrCaptor.capture())
-        ).thenAnswer {
-            enhetsregisteret[orgnrCaptor.value]
-        }
-    }
-
-    @Test
-    fun `Ingen rettigheter`() {
-        `when`(nærmestelederRepository.virksomheterSomNærmesteLeder("42")).thenReturn(listOf())
-        `when`(sykmeldingRepository.oversiktSykmeldinger("42")).thenReturn(mapOf())
-
-        val jsonResponse = mockMvc
-            .perform(get("/api/narmesteleder/virksomheter-v2").accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
-            .andExpect(status().isOk)
-            .andReturn().response.contentAsString
-        assertEquals("[]", jsonResponse)
-    }
-
-    @Test
-    fun `Ingen rettigheter, selv om det finnes sykmeldinger i virksomheten`() {
-        `when`(nærmestelederRepository.virksomheterSomNærmesteLeder("42")).thenReturn(listOf())
-        `when`(sykmeldingRepository.oversiktSykmeldinger("42")).thenReturn(mapOf("42" to 1))
-
-        val jsonResponse = mockMvc
-            .perform(get("/api/narmesteleder/virksomheter-v2").accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
-            .andExpect(status().isOk)
-            .andReturn().response.contentAsString
-        assertEquals("[]", jsonResponse)
-    }
 
     @Test
     fun `Er nærmeste leder, med sykmeldinger registrert`() {
-        `when`(nærmestelederRepository.virksomheterSomNærmesteLeder("42")).thenReturn(listOf())
+        `when`(authenticatedUserHolder.fnr).thenReturn("42")
         `when`(digisyfoService.hentVirksomheterOgSykmeldte("42")).thenReturn(
             listOf(
                 DigisyfoController.VirksomhetOgAntallSykmeldte(mkUnderenhet("10", "1"), 0),
@@ -170,69 +99,6 @@ class DigisyfoControllerTest {
         )
     }
 
-    @Test
-    fun `Er nærmeste leder, med sykmeldinger registrert, bakoverkompatibel`() {
-        `when`(nærmestelederRepository.virksomheterSomNærmesteLeder("42")).thenReturn(listOf("10", "20", "30"))
-        `when`(sykmeldingRepository.oversiktSykmeldinger("42")).thenReturn(mapOf("10" to 1, "20" to 2))
-
-        val jsonResponse = mockMvc
-            .perform(get("/api/narmesteleder/virksomheter-v2").accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
-            .andExpect(status().isOk)
-            .andReturn().response.contentAsString
-        JSONAssert.assertEquals(
-            """
-            [
-              {
-                "organisasjon": {
-                  "Name": "underenhet",
-                  "Type": null,
-                  "ParentOrganizationNumber": "1",
-                  "OrganizationNumber": "10",
-                  "OrganizationForm": "BEDR",
-                  "Status": null
-                },
-                "antallSykmeldinger": 1
-              },
-              {
-                "organisasjon": {
-                  "Name": "overenhet",
-                  "Type": null,
-                  "ParentOrganizationNumber": null,
-                  "OrganizationNumber": "1",
-                  "OrganizationForm": "AS",
-                  "Status": null
-                },
-                "antallSykmeldinger": 0
-              },
-              {
-                "organisasjon": {
-                  "Name": "underenhet",
-                  "Type": null,
-                  "ParentOrganizationNumber": "2",
-                  "OrganizationNumber": "20",
-                  "OrganizationForm": "BEDR",
-                  "Status": null
-                },
-                "antallSykmeldinger": 2
-              },
-              {
-                "organisasjon": {
-                  "Name": "overenhet",
-                  "Type": null,
-                  "ParentOrganizationNumber": null,
-                  "OrganizationNumber": "2",
-                  "OrganizationForm": "AS",
-                  "Status": null
-                },
-                "antallSykmeldinger": 0
-              }
-            ]
-            """,
-            jsonResponse,
-            true
-        )
-    }
 
     private fun mkUnderenhet(orgnr: String, parentOrgnr: String) =
         Organisasjon(
