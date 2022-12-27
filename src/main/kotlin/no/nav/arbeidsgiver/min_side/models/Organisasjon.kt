8a0e15bf04d7c5ddca9e6f2f00068d7b9fe5c38f
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-27 12:24:52 +0100

    konverter Organisasjon til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/models/Organisasjon.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/models/Organisasjon.kt
@@ -1,30 +1,62 @@
-package no.nav.arbeidsgiver.min_side.models;
+package no.nav.arbeidsgiver.min_side.models
 
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Organisasjon {
-    @JsonProperty("Name")
-    String name;
-    @JsonProperty("Type")
-    String type;
-    @JsonProperty("ParentOrganizationNumber")
-    String parentOrganizationNumber;
-    @JsonProperty("OrganizationNumber")
-    public String organizationNumber;
-    @JsonProperty("OrganizationForm")
-    String organizationForm;
-    @JsonProperty("Status")
-    String status;
+data class Organisasjon(
+    @field:JsonProperty("Name") var name: String? = null,
+    @field:JsonProperty("Type") var type: String? = null,
+    @field:JsonProperty("ParentOrganizationNumber") var parentOrganizationNumber: String? = null,
+    @field:JsonProperty("OrganizationNumber") var organizationNumber: String? = null,
+    @field:JsonProperty("OrganizationForm") var organizationForm: String? = null,
+    @field:JsonProperty("Status") var status: String? = null
+) {
+    companion object {
+        @JvmStatic
+        fun builder() = OrgBuilder()
+    }
+
+    // TODO: fjern denne når alt er konvertert til kotlin
+    class OrgBuilder(
+        var name: String? = null,
+        var type: String? = null,
+        var parentOrganizationNumber: String? = null,
+        var organizationNumber: String? = null,
+        var organizationForm: String? = null,
+        var status: String? = null
+    ) {
+        fun name(name: String?): OrgBuilder {
+            this.name = name
+            return this
+        }
+        fun type(type: String?): OrgBuilder {
+            this.type = type
+            return this
+        }
+        fun parentOrganizationNumber(parentOrganizationNumber: String?): OrgBuilder {
+            this.parentOrganizationNumber = parentOrganizationNumber
+            return this
+        }
+        fun organizationNumber(organizationNumber: String?): OrgBuilder {
+            this.organizationNumber = organizationNumber
+            return this
+        }
+        fun organizationForm(organizationForm: String?): OrgBuilder {
+            this.organizationForm = organizationForm
+            return this
+        }
+        fun status(status: String?): OrgBuilder {
+            this.status = status
+            return this
+        }
+        fun build() = Organisasjon(
+            name = name,
+            type = type,
+            parentOrganizationNumber = parentOrganizationNumber,
+            organizationNumber = organizationNumber,
+            organizationForm = organizationForm,
+            status = status,
+        )
+    }
 }
