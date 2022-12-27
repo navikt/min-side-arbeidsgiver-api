078552a8ea71357ca2337e0365f4200dd133842d
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-27 14:35:35 +0100

    konverter SwaggerConfig til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/SwaggerConfig.kt
@@ -1,15 +1,12 @@
-package no.nav.arbeidsgiver.min_side;
+package no.nav.arbeidsgiver.min_side
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class SwaggerConfig {
+class SwaggerConfig {
     @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Ditt Nav Arbeidsgiver API"));
-    }
+    fun springShopOpenAPI(): OpenAPI = OpenAPI().info(Info().title("Ditt Nav Arbeidsgiver API"))
 }
