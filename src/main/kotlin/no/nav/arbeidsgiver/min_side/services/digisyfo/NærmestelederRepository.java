c929d115c2adb14a7551e54746ff3729852d57d1
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-28 13:18:19 +0100

    slett ubrukt repository kode

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/services/digisyfo/NærmestelederRepository.java
+++
@@ -1,7 +0,0 @@
-package no.nav.arbeidsgiver.min_side.services.digisyfo;
-
-import java.util.List;
-
-public interface NærmestelederRepository {
-    List<String> virksomheterSomNærmesteLeder(String lederFnr);
-}
