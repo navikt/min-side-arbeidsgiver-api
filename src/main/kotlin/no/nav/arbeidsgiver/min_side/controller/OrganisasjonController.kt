3afa6fcd5468c233d968f3f03f12e41a09b3a490
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-28 09:51:52 +0100

    konverter OrganisasjonController til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/OrganisasjonController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/OrganisasjonController.kt
@@ -1,49 +1,31 @@
-package no.nav.arbeidsgiver.min_side.controller;
+package no.nav.arbeidsgiver.min_side.controller
 
-import lombok.extern.slf4j.Slf4j;
-import no.nav.security.token.support.core.api.ProtectedWithClaims;
-import no.nav.arbeidsgiver.min_side.models.Organisasjon;
-import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-import java.util.List;
-
-import static no.nav.arbeidsgiver.min_side.controller.AuthenticatedUserHolder.*;
-
-@ProtectedWithClaims(issuer = TOKENX, claimMap = {REQUIRED_LOGIN_LEVEL})
-@Slf4j
+@ProtectedWithClaims(
+    issuer = AuthenticatedUserHolder.TOKENX,
+    claimMap = [AuthenticatedUserHolder.REQUIRED_LOGIN_LEVEL]
+)
 @RestController
-public class OrganisasjonController {
-
-    private final AltinnService altinnService;
-    private final AuthenticatedUserHolder authenticatedUserHolder;
-
-    @Autowired
-    public OrganisasjonController(
-            AltinnService altinnService,
-            AuthenticatedUserHolder authenticatedUserHolder
-    ) {
-        this.altinnService = altinnService;
-        this.authenticatedUserHolder = authenticatedUserHolder;
-    }
-
+class OrganisasjonController(
+    val altinnService: AltinnService,
+    val authenticatedUserHolder: AuthenticatedUserHolder,
+) {
     @GetMapping("/api/organisasjoner")
-    public List<Organisasjon> hentOrganisasjoner() {
-        return altinnService.hentOrganisasjoner(authenticatedUserHolder.getFnr());
-    }
+    fun hentOrganisasjoner() =
+        altinnService.hentOrganisasjoner(authenticatedUserHolder.fnr)
 
     @GetMapping("/api/rettigheter-til-skjema")
-    public List<Organisasjon> hentRettigheter(
-            @RequestParam String serviceKode,
-            @RequestParam String serviceEdition
-    ){
-        return altinnService.hentOrganisasjonerBasertPaRettigheter(
-                authenticatedUserHolder.getFnr(),
-                serviceKode,
-                serviceEdition
-        );
-    }
+    fun hentRettigheter(
+        @RequestParam serviceKode: String,
+        @RequestParam serviceEdition: String,
+    ) = altinnService.hentOrganisasjonerBasertPaRettigheter(
+        authenticatedUserHolder.fnr,
+        serviceKode,
+        serviceEdition
+    )
 }
---
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/controller/OrganisasjonControllerTest.kt
@@ -0,0 +1,135 @@
+package no.nav.arbeidsgiver.min_side.controller
+
+import no.nav.arbeidsgiver.min_side.models.Organisasjon
+import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.skyscreamer.jsonassert.JSONAssert
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(
+    value = [OrganisasjonController::class],
+    properties = [
+        "server.servlet.context-path=/",
+        "tokensupport.enabled=false",
+    ]
+)
+class OrganisasjonControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var authenticatedUserHolder: AuthenticatedUserHolder
+
+    @MockBean
+    lateinit var altinnService: AltinnService
+
+    @Test
+    fun hentOrganisasjoner() {
+        `when`(authenticatedUserHolder.fnr).thenReturn("42")
+        `when`(altinnService.hentOrganisasjoner("42")).thenReturn(
+            listOf(
+                Organisasjon(
+                    name = "underenhet",
+                    parentOrganizationNumber = "1",
+                    organizationNumber = "10",
+                    organizationForm = "BEDR"
+                ),
+                Organisasjon(
+                    name = "overenhet",
+                    organizationNumber = "1",
+                    organizationForm = "AS"
+                ),
+            )
+        )
+
+        val jsonResponse = mockMvc
+            .perform(get("/api/organisasjoner").accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
+        JSONAssert.assertEquals(
+            """
+            [
+              {
+                  "Name": "underenhet",
+                  "Type": null,
+                  "ParentOrganizationNumber": "1",
+                  "OrganizationNumber": "10",
+                  "OrganizationForm": "BEDR",
+                  "Status": null
+              },
+              {
+                  "Name": "overenhet",
+                  "Type": null,
+                  "ParentOrganizationNumber": null,
+                  "OrganizationNumber": "1",
+                  "OrganizationForm": "AS",
+                  "Status": null
+              }
+            ]
+            """,
+            jsonResponse,
+            true
+        )
+    }
+
+    @Test
+    fun hentRettigheter() {
+        val serviceKode = "1234"
+        val serviceEdition = "1"
+        `when`(authenticatedUserHolder.fnr).thenReturn("42")
+        `when`(altinnService.hentOrganisasjonerBasertPaRettigheter("42", serviceKode, serviceEdition)).thenReturn(
+            listOf(
+                Organisasjon(
+                    name = "underenhet",
+                    parentOrganizationNumber = "1",
+                    organizationNumber = "10",
+                    organizationForm = "BEDR"
+                ),
+                Organisasjon(
+                    name = "overenhet",
+                    organizationNumber = "1",
+                    organizationForm = "AS"
+                ),
+            )
+        )
+
+        val jsonResponse = mockMvc
+            .perform(get("/api/rettigheter-til-skjema?serviceKode=$serviceKode&serviceEdition=$serviceEdition").accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
+        JSONAssert.assertEquals(
+            """
+            [
+              {
+                  "Name": "underenhet",
+                  "Type": null,
+                  "ParentOrganizationNumber": "1",
+                  "OrganizationNumber": "10",
+                  "OrganizationForm": "BEDR",
+                  "Status": null
+              },
+              {
+                  "Name": "overenhet",
+                  "Type": null,
+                  "ParentOrganizationNumber": null,
+                  "OrganizationNumber": "1",
+                  "OrganizationForm": "AS",
+                  "Status": null
+              }
+            ]
+            """,
+            jsonResponse,
+            true
+        )
+    }
+}
