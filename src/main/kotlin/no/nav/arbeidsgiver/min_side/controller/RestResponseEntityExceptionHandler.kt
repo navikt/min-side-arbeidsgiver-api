05f05a69624ead89ebc049ef80e08488c2086f01
Ken Gullaksen <ken.gullaksen@nav.no>
2022-12-28 10:12:52 +0100

    konverter RestResponseEntityExceptionHandler til kotlin

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/controller/RestResponseEntityExceptionHandler.kt
@@ -1,108 +1,93 @@
-package no.nav.arbeidsgiver.min_side.controller;
+package no.nav.arbeidsgiver.min_side.controller
 
-import io.ktor.client.plugins.ServerResponseException;
-import io.ktor.http.HttpStatusCode;
-import lombok.Data;
-import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.error.exceptions.AltinnrettigheterProxyKlientFallbackException;
-import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.net.SocketTimeoutException;
-import java.util.List;
-
-import static java.lang.String.format;
-import static java.lang.invoke.MethodHandles.lookup;
+import io.ktor.client.plugins.*
+import io.ktor.http.HttpStatusCode.Companion.BadGateway
+import io.ktor.http.HttpStatusCode.Companion.GatewayTimeout
+import io.ktor.http.HttpStatusCode.Companion.ServiceUnavailable
+import no.nav.arbeidsgiver.altinnrettigheter.proxy.klient.error.exceptions.AltinnrettigheterProxyKlientFallbackException
+import no.nav.arbeidsgiver.min_side.config.logger
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.client.HttpClientErrorException.Forbidden
+import org.springframework.web.client.HttpClientErrorException.Unauthorized
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.net.SocketTimeoutException
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+    private val log = logger()
 
-    private static final Logger logger = LoggerFactory.getLogger(lookup().lookupClass());
-
-    @ExceptionHandler({RuntimeException.class})
+    @ExceptionHandler(RuntimeException::class)
     @ResponseBody
-    protected ResponseEntity<Object> handleInternalError(RuntimeException e, WebRequest ignored) {
-        logger.error("Uhåndtert feil: {}", e.getMessage(), e);
-        return getResponseEntity(e, "Internal error", HttpStatus.INTERNAL_SERVER_ERROR);
+    fun handleInternalError(e: RuntimeException, ignored: WebRequest?): ResponseEntity<Any> {
+        log.error("Uhåndtert feil: {}", e.message, e)
+        return getResponseEntity(e, "Internal error", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
-    @ExceptionHandler({AltinnrettigheterProxyKlientFallbackException.class})
+    @ExceptionHandler(AltinnrettigheterProxyKlientFallbackException::class)
     @ResponseBody
-    protected ResponseEntity<Object> handleAltinnFallbackFeil(AltinnrettigheterProxyKlientFallbackException e, WebRequest ignored) {
-        if (e.getCause() instanceof SocketTimeoutException) {
-            return getResponseEntity(e, "Fallback til Altinn feilet pga timeout", HttpStatus.GATEWAY_TIMEOUT);
+    fun handleAltinnFallbackFeil(
+        e: AltinnrettigheterProxyKlientFallbackException,
+        ignored: WebRequest?
+    ): ResponseEntity<Any> {
+        if (e.cause is SocketTimeoutException) {
+            return getResponseEntity(e, "Fallback til Altinn feilet pga timeout", HttpStatus.GATEWAY_TIMEOUT)
         }
-
-        HttpStatus httpStatus = hentDriftsforstyrrelse(e);
-        if (httpStatus != null) {
-            return getResponseEntity(e, "Fallback til Altinn feilet pga driftsforstyrrelse", httpStatus);
+        val httpStatus = hentDriftsforstyrrelse(e)
+        return if (httpStatus != null) {
+            getResponseEntity(e, "Fallback til Altinn feilet pga driftsforstyrrelse", httpStatus)
         } else {
-            return handleInternalError(e, ignored);
+            handleInternalError(e, ignored)
         }
     }
 
-    private HttpStatus hentDriftsforstyrrelse(AltinnrettigheterProxyKlientFallbackException e) {
-        if (e.getCause() instanceof ServerResponseException) {
-            ServerResponseException serverResponseException = (ServerResponseException) e.getCause();
-            HttpStatusCode status = serverResponseException.getResponse().getStatus();
-            boolean erDriftsforstyrrelse = List.of(
-                    HttpStatusCode.Companion.getBadGateway(),
-                    HttpStatusCode.Companion.getServiceUnavailable(),
-                    HttpStatusCode.Companion.getGatewayTimeout()
-            ).contains(status);
-            return erDriftsforstyrrelse ? HttpStatus.valueOf(status.getValue()) : null;
-        } else {
-            return null;
+    @ExceptionHandler(Forbidden::class)
+    @ResponseBody
+    fun handleForbidden(e: RuntimeException, ignored: WebRequest?) =
+        getResponseEntity(e, "ingen tilgang", HttpStatus.FORBIDDEN)
+
+    @ExceptionHandler(JwtTokenUnauthorizedException::class, Unauthorized::class)
+    @ResponseBody
+    fun handleUnauthorized(e: RuntimeException, ignored: WebRequest?) =
+        getResponseEntity(e, "ingen tilgang", HttpStatus.UNAUTHORIZED)
+
+
+    private fun hentDriftsforstyrrelse(e: AltinnrettigheterProxyKlientFallbackException): HttpStatus? {
+        return when (e.cause) {
+            is ServerResponseException -> {
+                val serverResponseException = e.cause as ServerResponseException
+                val status = serverResponseException.response.status
+                val erDriftsforstyrrelse = listOf(
+                    BadGateway,
+                    ServiceUnavailable,
+                    GatewayTimeout
+                ).contains(status)
+                if (erDriftsforstyrrelse) HttpStatus.valueOf(status.value) else null
+            }
+
+            else -> null
         }
     }
 
-    @ExceptionHandler({
-            HttpClientErrorException.Forbidden.class,
-    })
-    @ResponseBody
-    protected ResponseEntity<Object> handleForbidden(RuntimeException e, WebRequest ignored) {
-        return getResponseEntity(e, "ingen tilgang", HttpStatus.FORBIDDEN);
+    private fun getResponseEntity(
+        t: Throwable,
+        melding: String,
+        status: HttpStatus
+    ): ResponseEntity<Any> {
+        val body = FeilRespons(melding, t.message)
+        log.info("Returnerer følgende HttpStatus '$status' med melding '$melding' pga exception '${t.message}'", t)
+        return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(body)
     }
 
-    @ExceptionHandler({
-            JwtTokenUnauthorizedException.class,
-            HttpClientErrorException.Unauthorized.class,
-    })
-    @ResponseBody
-    protected ResponseEntity<Object> handleUnauthorized(RuntimeException e, WebRequest ignored) {
-        return getResponseEntity(e, "ingen tilgang", HttpStatus.UNAUTHORIZED);
-    }
-
-    private ResponseEntity<Object> getResponseEntity(
-            Throwable t,
-            String melding,
-            HttpStatus status
-    ) {
-        FeilRespons body = new FeilRespons(melding, t.getMessage());
-        logger.info(
-                format(
-                        "Returnerer følgende HttpStatus '%s' med melding '%s' pga exception '%s'",
-                        status.toString(),
-                        melding,
-                        t.getMessage()
-                ), t
-        );
-        return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(body);
-    }
-
-    @Data
-    public static class FeilRespons {
-        final String message;
-        final String cause;
-    }
+    data class FeilRespons(
+        val message: String? = null,
+        val cause: String? = null
+    )
 
 }
