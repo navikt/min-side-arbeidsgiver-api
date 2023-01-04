2056ab38eb3c3c65db2b6766e1352b7ccd1355ec
Ken Gullaksen <ken.gullaksen@nav.no>
2023-01-04 10:10:51 +0100

    rewrite masking appender to work with logback 1.4 and joran

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/logging/MaskingAppender.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/logging/MaskingAppender.kt
@@ -2,31 +2,25 @@ package no.nav.arbeidsgiver.min_side.logging
 
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.classic.spi.IThrowableProxy
-import ch.qos.logback.core.Appender
-import ch.qos.logback.core.AppenderBase
+import ch.qos.logback.core.AsyncAppenderBase
 
-class MaskingAppender: AppenderBase<ILoggingEvent>() {
-
-    var appender: Appender<ILoggingEvent>? = null
-
+class MaskingAppender: AsyncAppenderBase<ILoggingEvent>() {
     override fun append(event: ILoggingEvent) {
-        appender?.doAppend(
-            object : ILoggingEvent by event {
-                override fun getFormattedMessage(): String? =
-                    mask(event.formattedMessage)
+        super.append(object : ILoggingEvent by event {
+            override fun getFormattedMessage(): String? =
+                mask(event.formattedMessage)
 
-                override fun getThrowableProxy(): IThrowableProxy? {
-                    if (event.throwableProxy == null) {
-                        return null
-                    }
-                    return object : IThrowableProxy by event.throwableProxy {
-                        override fun getMessage(): String? =
-                            mask(event.throwableProxy.message)
-                    }
+            override fun getThrowableProxy(): IThrowableProxy? {
+                if (event.throwableProxy == null) {
+                    return null
                 }
-
+                return object : IThrowableProxy by event.throwableProxy {
+                    override fun getMessage(): String? =
+                        mask(event.throwableProxy.message)
+                }
             }
-        )
+
+        })
     }
 
     companion object {
