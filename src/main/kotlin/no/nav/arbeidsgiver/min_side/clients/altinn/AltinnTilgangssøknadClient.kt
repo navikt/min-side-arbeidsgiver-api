9a4e237f304e9f143f72554404c69f56e01c0291
Ken Gullaksen <ken.gullaksen@nav.no>
2023-01-09 17:07:10 +0100

    legg til warn logg med litt detaljer når vi får bad request fra altinn

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/clients/altinn/AltinnTilgangssøknadClient.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/clients/altinn/AltinnTilgangssøknadClient.kt
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException.BadGateway
 import org.springframework.web.util.UriComponentsBuilder
 import java.util.function.Consumer
@@ -105,7 +106,12 @@ class AltinnTilgangssøknadClient(
             .post("$delegationRequestApiPath?ForceEIAuthentication")
             .headers(altinnHeaders)
             .body(delegationRequest)
-        val response = restTemplate.exchange(request, object : ParameterizedTypeReference<DelegationRequest?>() {})
+        val response = try {
+            restTemplate.exchange(request, object : ParameterizedTypeReference<DelegationRequest?>() {})
+        } catch (e: HttpClientErrorException.BadRequest) {
+            log.warn("sendSøknad feilet for delegationRequest.RequestResources={}. e={}", delegationRequest.RequestResources, e.message)
+            throw e
+        }
         val body = response.body
         val svar = AltinnTilgangssøknad()
         svar.status = body!!.RequestStatus
