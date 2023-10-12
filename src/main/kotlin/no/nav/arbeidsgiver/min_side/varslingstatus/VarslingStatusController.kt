d8e58a66c84a57eab7a13a0b2c6db062371346e5
Ken Gullaksen <ken.gullaksen@nav.no>
2023-10-12 13:03:26 +0200

    fjern test i dev

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/varslingstatus/VarslingStatusController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/varslingstatus/VarslingStatusController.kt
@@ -1,6 +1,5 @@
 package no.nav.arbeidsgiver.min_side.varslingstatus
 
-import no.nav.arbeidsgiver.min_side.config.GittMiljø
 import no.nav.arbeidsgiver.min_side.controller.AuthenticatedUserHolder
 import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
 import no.nav.arbeidsgiver.min_side.varslingstatus.VarslingStatusDto.Status
@@ -15,21 +14,7 @@ class VarslingStatusController(
     private val authenticatedUserHolder: AuthenticatedUserHolder,
     private val altinnService: AltinnService,
     private val repository: VarslingStatusRepository,
-    private val gittMiljø: GittMiljø,
 ) {
-
-    /**
-     * midlertidig simuler mangler kofuvi i dev da dette mangler.
-     * TODO: fjern før merge
-     */
-    fun mapStatus(varslingStatus: VarslingStatus) = gittMiljø.resolve(
-        dev = {
-            varslingStatus.copy(status = Status.MANGLER_KOFUVI)
-        },
-        other = {
-            varslingStatus
-        },
-    )
 
     @PostMapping("/api/varslingStatus/v1")
     fun getVarslingStatus(@RequestBody requestBody: VarslingStatusRequest): VarslingStatus {
@@ -38,14 +23,14 @@ class VarslingStatusController(
             .any { it.organizationNumber == virksomhetsnummer }
 
         if (!harTilgang) {
-            return mapStatus(VarslingStatus(
+            return VarslingStatus(
                 status = Status.OK,
                 varselTimestamp = LocalDateTime.now(),
                 kvittertEventTimestamp = Instant.now(),
-            ))
+            )
         }
 
-        return mapStatus(repository.varslingStatus(virksomhetsnummer = virksomhetsnummer))
+        return repository.varslingStatus(virksomhetsnummer = virksomhetsnummer)
     }
 
     data class VarslingStatusRequest(
