ec814ad69c026a6389a9ef5fbd2e599ff31af706
Ken Gullaksen <ken.gullaksen@nav.no>
2023-11-24 11:36:57 +0100

    bump springboot 3.2 og fjern token-support-test

--- src/test/kotlin/no/nav/arbeidsgiver/min_side/controller/TilgangsstyringUtføresTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/controller/TilgangsstyringUtføresTest.kt
@@ -1,36 +1,31 @@
 package no.nav.arbeidsgiver.min_side.controller
 
-import com.nimbusds.jose.JOSEObjectType
-import no.nav.security.mock.oauth2.MockOAuth2Server
-import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
-import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 
 @SpringBootTest(
     properties = [
-        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8118/issuer1"
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:8080/fake",
+        "spring.security.oauth2.resourceserver.jwt.audiences=someaudience"
     ]
 )
 @AutoConfigureMockMvc
 @ActiveProfiles("local")
-@EnableMockOAuth2Server(port = 8118)
 class TilgangsstyringUtføresTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
     @Autowired
-    lateinit var server: MockOAuth2Server
+    lateinit var restClientBuilder: RestClient.Builder
 
     @Test
     fun tilgangsstyringUtføres() {
@@ -42,7 +37,7 @@ class TilgangsstyringUtføresTest {
 
     @Test
     fun tilgangsstyringErOkForAcrLevel4() {
-        val token = token("42", mapOf("acr" to "Level4"))
+        val token = token("42", "Level4")
         mockMvc.get("/api/userInfo/v1") {
             header(AUTHORIZATION, "Bearer $token")
         }.andExpect {
@@ -52,7 +47,7 @@ class TilgangsstyringUtføresTest {
 
     @Test
     fun tilgangsstyringErOkForAcridportenLoaHigh() {
-        val token = token("42", mapOf("acr" to "idporten-loa-high"))
+        val token = token("42", "idporten-loa-high")
         mockMvc.get("/api/userInfo/v1") {
             header(AUTHORIZATION, "Bearer $token")
         }.andExpect {
@@ -60,18 +55,6 @@ class TilgangsstyringUtføresTest {
         }
     }
 
-    private fun token(subject: String, claims: Map<String, String>): String? {
-        return server.issueToken(
-            "issuer1",
-            "theclientid",
-            DefaultOAuth2TokenCallback(
-                "issuer1",
-                subject,
-                JOSEObjectType.JWT.type,
-                listOf("someaudience"),
-                claims + mapOf("pid" to subject),
-                3600
-            )
-        ).serialize()
-    }
+    private fun token(pid: String, acr: String): String? =
+        restClientBuilder.build().get().uri("http://localhost:8080/fake/tokenx?client_id=someclientid&aud=someaudience&pid=$pid&acr=$acr").retrieve().body()
 }
