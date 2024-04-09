56bf25040ea0e8231dc724d911afdd22cc8a7ea4
Ken Gullaksen <ken.gullaksen@nav.no>
2024-04-09 10:56:20 +0200

    legg til api for kontostatus
    følger samme mønster som api for varslingstatus
    forutsetter at sokos-kontoregister kan nås fra gcp og at vår klient er autentisert
    koden her har ikke autentisering med, det må legges til

---
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/kontostatus/KontostatusTest.kt
@@ -0,0 +1,100 @@
+package no.nav.arbeidsgiver.min_side.kontostatus
+
+import no.nav.arbeidsgiver.min_side.controller.SecurityMockMvcUtil.Companion.jwtWithPid
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.method
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
+import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest(
+    properties = [
+        "server.servlet.context-path=/",
+        "spring.flyway.cleanDisabled=false",
+    ]
+)
+@AutoConfigureMockMvc
+class KontostatusTest {
+
+    @Autowired
+    lateinit var kontoregisterClient: KontoregisterClient
+
+    lateinit var server: MockRestServiceServer
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setUp() {
+        server = MockRestServiceServer.bindTo(kontoregisterClient.restTemplate).build()
+    }
+
+
+    @Test
+    fun `henter kontonummer fra kontoregister`() {
+        val virksomhetsnummer = "42"
+        server.expect {
+            requestTo("/kontoregister/api/v1/hent-kontonummer-for-organisasjon/$virksomhetsnummer")
+            method(GET)
+        }.andRespond(
+            withSuccess(
+                """
+                {
+                    "mottaker": "42",
+                    "kontonr": "12345678901"
+                }
+                """,
+                APPLICATION_JSON
+            )
+        )
+
+        kontoregisterClient.hentKontonummer(virksomhetsnummer).let {
+            Assertions.assertEquals("42", it?.mottaker)
+            Assertions.assertEquals("12345678901", it?.kontonr)
+        }
+
+        mockMvc.get("/api/kontonummerStatus/v1") {
+            content = """{"virksomhetsnummer": "$virksomhetsnummer"}"""
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+            with(jwtWithPid("42"))
+        }.andExpect {
+            status { isOk() }
+            content { json("""{"status": "OK"}""") }
+        }
+    }
+
+    @Test
+    fun `finner ikke kontonummer for virksomhet`() {
+        val virksomhetsnummer = "123"
+        server.expect {
+            requestTo("/kontoregister/api/v1/hent-kontonummer-for-organisasjon/$virksomhetsnummer")
+            method(GET)
+        }.andRespond(withStatus(NOT_FOUND))
+
+        kontoregisterClient.hentKontonummer(virksomhetsnummer).let {
+            Assertions.assertNull(it)
+        }
+
+        mockMvc.get("/api/kontonummerStatus/v1") {
+            content = """{"virksomhetsnummer": "$virksomhetsnummer"}"""
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+            with(jwtWithPid("42"))
+        }.andExpect {
+            status { isOk() }
+            content { json("""{"status": "MANGLER_KONTONUMMER"}""") }
+        }
+    }
+}
