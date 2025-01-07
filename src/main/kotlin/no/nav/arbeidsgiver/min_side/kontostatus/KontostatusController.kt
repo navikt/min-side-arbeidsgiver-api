aabcee723a71ee5a7183fc756d076b598c681cb5
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-01-07 12:50:50 +0100

    Legg til endepunkt for å hente kontonummer

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/kontostatus/KontostatusController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/kontostatus/KontostatusController.kt
@@ -1,24 +1,55 @@
 package no.nav.arbeidsgiver.min_side.kontostatus
 
+import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
+
+const val kontonummerTilgangTjenesetekode = "2896:87"
 
 @RestController
 class KontostatusController(
     val kontoregisterClient: KontoregisterClient,
+    val altinnService: AltinnService
 ) {
+
     @PostMapping("/api/kontonummerStatus/v1")
-    fun get(
+    fun getKontonummerStatus(
         @RequestBody body: Request,
     ) = when (kontoregisterClient.hentKontonummer(body.virksomhetsnummer)) {
         null -> Response(KontonummerStatus.MANGLER_KONTONUMMER)
         else -> Response(KontonummerStatus.OK)
     }
 
+    @PostMapping("/api/kontonummer/v1")
+    fun getKontoNummer(
+        @RequestBody body: Request,
+    ): Response {
+        val harTilgang = altinnService.harTilgang(body.virksomhetsnummer, kontonummerTilgangTjenesetekode)
+        if (!harTilgang) {
+            throw HttpClientErrorException.Unauthorized.create(
+                HttpStatus.UNAUTHORIZED,
+                "Unauthorized",
+                org.springframework.http.HttpHeaders.EMPTY,
+                byteArrayOf(),
+                null
+            )
+        }
+        return when (val oppslag = kontoregisterClient.hentKontonummer(body.virksomhetsnummer)) {
+            null -> Response(KontonummerStatus.MANGLER_KONTONUMMER)
+            else -> Response(status = KontonummerStatus.OK, kontonummer = oppslag.kontonr, orgnr = oppslag.mottaker)
+        }
+    }
+
     data class Request(val virksomhetsnummer: String)
 
-    data class Response(val status: KontonummerStatus)
+    data class Response(
+        val status: KontonummerStatus,
+        val kontonummer: String? = null,
+        val orgnr: String? = null
+    )
 
     enum class KontonummerStatus {
         OK,
--- src/test/kotlin/no/nav/arbeidsgiver/min_side/kontostatus/KontostatusTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/kontostatus/KontostatusTest.kt
@@ -2,9 +2,11 @@ package no.nav.arbeidsgiver.min_side.kontostatus
 
 import no.nav.arbeidsgiver.min_side.azuread.AzureService
 import no.nav.arbeidsgiver.min_side.controller.SecurityMockMvcUtil.Companion.jwtWithPid
+import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -35,6 +37,9 @@ class KontostatusTest {
 
     @MockBean
     lateinit var azureService: AzureService
+
+    @MockBean
+    lateinit var altinnService: AltinnService
 
     @Autowired
     lateinit var kontoregisterClient: KontoregisterClient
@@ -104,6 +109,98 @@ class KontostatusTest {
         }.andExpect {
             status { isOk() }
             content { json("""{"status": "MANGLER_KONTONUMMER"}""") }
+        }
+    }
+
+    @Test
+    fun `henter kontonummer fra kontoregister og returnerer kontonummer og orgnr`() {
+        val virksomhetsnummer = "42"
+        server.expect {
+            requestTo("/kontoregister/api/v1/hent-kontonummer-for-organisasjon/$virksomhetsnummer")
+            method(POST)
+        }.andRespond(
+            withSuccess(
+                """
+                {
+                    "mottaker": "42",
+                    "kontonr": "12345678901"
+                }
+                """,
+                APPLICATION_JSON
+            )
+        )
+
+        kontoregisterClient.hentKontonummer(virksomhetsnummer).let {
+            Assertions.assertEquals("42", it?.mottaker)
+            Assertions.assertEquals("12345678901", it?.kontonr)
+        }
+        `when`(altinnService.harTilgang(virksomhetsnummer, "2896:87")).thenReturn(true)
+        mockMvc.post("/api/kontonummer/v1") {
+            content = """{"virksomhetsnummer": "$virksomhetsnummer"}"""
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+            with(jwtWithPid("42"))
+        }.andExpect {
+            status { isOk() }
+            content { json("""{"status": "OK", "orgnr": "42", "kontonummer": "12345678901"}""") }
+        }
+    }
+
+    @Test
+    fun `bruker har ikke tilgang til å se kontonummer returnerer unauthorized`() {
+        val virksomhetsnummer = "42"
+        server.expect {
+            requestTo("/kontoregister/api/v1/hent-kontonummer-for-organisasjon/$virksomhetsnummer")
+            method(POST)
+        }.andRespond(
+            withSuccess(
+                """
+                {
+                    "mottaker": "42",
+                    "kontonr": "12345678901"
+                }
+                """,
+                APPLICATION_JSON
+            )
+        )
+
+        kontoregisterClient.hentKontonummer(virksomhetsnummer).let {
+            Assertions.assertEquals("42", it?.mottaker)
+            Assertions.assertEquals("12345678901", it?.kontonr)
+        }
+        `when`(altinnService.harTilgang(virksomhetsnummer, "2896:87")).thenReturn(false)
+        mockMvc.post("/api/kontonummer/v1") {
+            content = """{"virksomhetsnummer": "$virksomhetsnummer"}"""
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+            with(jwtWithPid("42"))
+        }.andExpect {
+            status { isUnauthorized() }
+        }
+    }
+
+    @Test
+    fun `kontnummer finnes ikke for virksomhet`() {
+        val virksomhetsnummer = "123"
+        server.expect {
+            requestTo("/kontoregister/api/v1/hent-kontonummer-for-organisasjon/$virksomhetsnummer")
+            method(POST)
+        }.andRespond(withStatus(NOT_FOUND))
+
+        kontoregisterClient.hentKontonummer(virksomhetsnummer).let {
+            Assertions.assertNull(it)
+        }
+
+        `when`(altinnService.harTilgang(virksomhetsnummer, "2896:87")).thenReturn(true)
+
+        mockMvc.post("/api/kontonummerStatus/v1") {
+            content = """{"virksomhetsnummer": "$virksomhetsnummer"}"""
+            contentType = APPLICATION_JSON
+            accept = APPLICATION_JSON
+            with(jwtWithPid("123"))
+        }.andExpect {
+            status { isOk() }
+            content { json("""{"status": "MANGLER_KONTONUMMER", "orgnr":  null, "kontonummer": null}""") }
         }
     }
 }
