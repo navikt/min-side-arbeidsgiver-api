630921ab815e1f26196e541f998a11a7fbaab0cf
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-07-02 11:22:36 +0200

    add @Profile(!test)

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/services/lagredefilter/MigrateLagredeFilterJob.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/services/lagredefilter/MigrateLagredeFilterJob.kt
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.arbeidsgiver.min_side.config.logger
 import no.nav.arbeidsgiver.min_side.services.storage.StorageEntry
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
@@ -23,7 +24,8 @@ class MigrateLagredeFilterJob(
 ) {
     private val log = logger()
 
-//    @EventListener(ApplicationReadyEvent::class)
+    @Profile("!test")
+    @EventListener(ApplicationReadyEvent::class)
     @Transactional
     fun run() {
         try {
