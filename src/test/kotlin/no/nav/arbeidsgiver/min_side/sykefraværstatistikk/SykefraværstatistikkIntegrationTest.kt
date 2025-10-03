a1317f5e57bb00fe01cf07b74f5a255524e86cf7
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-10-03 13:14:37 +0200

    Fiks deserialize test

--- src/test/kotlin/no/nav/arbeidsgiver/min_side/sykefraværstatistikk/SykefraværstatistikkIntegrationTest.kt
+++ src/test/kotlin/no/nav/arbeidsgiver/min_side/sykefraværstatistikk/SykefraværstatistikkIntegrationTest.kt
@@ -1,47 +1,25 @@
 package no.nav.arbeidsgiver.min_side.sykefraværstatistikk
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.ktor.client.request.bearerAuth
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.plugins.di.dependencies
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.plugins.di.*
 import no.nav.arbeidsgiver.min_side.FakeApi
 import no.nav.arbeidsgiver.min_side.FakeApplication
-import no.nav.arbeidsgiver.min_side.controller.SecurityMockMvcUtil.Companion.jwtWithPid
 import no.nav.arbeidsgiver.min_side.fakeToken
 import no.nav.arbeidsgiver.min_side.provideDefaultObjectMapper
 import no.nav.arbeidsgiver.min_side.services.altinn.AltinnService
-import no.nav.arbeidsgiver.min_side.services.digisyfo.ConsumerRecordProcessor
-import no.nav.arbeidsgiver.min_side.services.digisyfo.MsaKafkaConsumer
 import no.nav.arbeidsgiver.min_side.services.digisyfo.SykefraværStatistikkMetadataRecordProcessor
 import no.nav.arbeidsgiver.min_side.services.digisyfo.SykefraværStatistikkRecordProcessor
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.flywaydb.core.Flyway
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.skyscreamer.jsonassert.JSONAssert.assertEquals
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.test.context.bean.override.mockito.MockitoBean
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.LocalDateTime
 
-//@SpringBootTest(
-//    properties = [
-//        "server.servlet.context-path=/",
-//        "spring.flyway.cleanDisabled=false",
-//    ]
-//)
-//@AutoConfigureMockMvc
 class SykefraværstatistikkIntegrationTest {
     companion object {
         @RegisterExtension
@@ -59,39 +37,6 @@ class SykefraværstatistikkIntegrationTest {
         @RegisterExtension
         val fakeApi = FakeApi()
     }
-
-//    @Autowired
-//    lateinit var mockMvc: MockMvc
-//
-//    @Autowired
-//    lateinit var sykefraværstatistikkRepository: SykefraværstatistikkRepository
-//
-//    @Autowired
-//    lateinit var objectMapper: ObjectMapper
-//
-//    lateinit var sykefraværstatistikkKafkaListener: SykefraværstatistikkKafkaListener
-//
-//
-//    @Suppress("unused")
-//    @MockitoBean // the real jwt decoder is bypassed by SecurityMockMvcRequestPostProcessors.jwt
-//    lateinit var jwtDecoder: JwtDecoder
-//
-//    @MockitoBean
-//    lateinit var altinnService: AltinnService
-//
-//    @Autowired
-//    lateinit var flyway: Flyway
-//
-//
-//    @BeforeEach
-//    fun setup() {
-//        flyway.clean()
-//        flyway.migrate()
-//        sykefraværstatistikkKafkaListener = SykefraværstatistikkKafkaListener(
-//            sykefraværstatistikkRepository,
-//            objectMapper,
-//        )
-//    }
 
     val innenværendeår = LocalDateTime.now().year
 
@@ -155,24 +100,6 @@ class SykefraværstatistikkIntegrationTest {
                 """, it.bodyAsText(), true
             )
         }
-
-//        mockMvc
-//            .perform(
-//                get("/api/sykefravaerstatistikk/{orgnr}", "123")
-//                    .with(jwtWithPid("42"))
-//            )
-//            .andExpect(status().isOk)
-//            .andReturn().response.contentAsString.also {
-//                assertEquals(
-//                    """
-//                    {
-//                        "type": "VIRKSOMHET",
-//                        "label": "123",
-//                        "prosent": 3.14
-//                    }
-//                """, it, true
-//                )
-//            }
     }
 
     @Test
@@ -287,24 +214,6 @@ class SykefraværstatistikkIntegrationTest {
                 """, it.bodyAsText(), true
             )
         }
-
-//        mockMvc
-//            .perform(
-//                get("/api/sykefravaerstatistikk/{orgnr}", "123")
-//                    .with(jwtWithPid("42"))
-//            )
-//            .andExpect(status().isOk)
-//            .andReturn().response.contentAsString.also {
-//                assertEquals(
-//                    """
-//                    {
-//                        "type": "BRANSJE",
-//                        "label": "Testing",
-//                        "prosent": 3.14
-//                    }
-//                """, it, true
-//                )
-//            }
     }
 
     @Test
@@ -374,24 +283,6 @@ class SykefraværstatistikkIntegrationTest {
                 """, it.bodyAsText(), true
             )
         }
-//
-//        mockMvc
-//            .perform(
-//                get("/api/sykefravaerstatistikk/{orgnr}", "123")
-//                    .with(jwtWithPid("42"))
-//            )
-//            .andExpect(status().isOk)
-//            .andReturn().response.contentAsString.also {
-//                assertEquals(
-//                    """
-//                    {
-//                        "type": "NÆRING",
-//                        "label": "IT",
-//                        "prosent": 3.14
-//                    }
-//                """, it, true
-//                )
-//            }
     }
 
 
@@ -514,24 +405,6 @@ class SykefraværstatistikkIntegrationTest {
                 """, it.bodyAsText(), true
             )
         }
-
-//        mockMvc
-//            .perform(
-//                get("/api/sykefravaerstatistikk/{orgnr}", "123")
-//                    .with(jwtWithPid("42"))
-//            )
-//            .andExpect(status().isOk)
-//            .andReturn().response.contentAsString.also {
-//                assertEquals(
-//                    """
-//                    {
-//                        "type": "BRANSJE",
-//                        "label": "Testing",
-//                        "prosent": 3.14
-//                    }
-//                """, it, true
-//                )
-//            }
     }
 
     @Test
@@ -556,13 +429,6 @@ class SykefraværstatistikkIntegrationTest {
         }.also {
             assert(HttpStatusCode.NoContent == it.status)
         }
-//
-//        mockMvc
-//            .perform(
-//                get("/api/sykefravaerstatistikk/{orgnr}", "123")
-//                    .with(jwtWithPid("42"))
-//            )
-//            .andExpect(status().isNoContent)
     }
 
     private suspend fun FakeApplication.processStatistikkkategori(key: String, value: String) {
