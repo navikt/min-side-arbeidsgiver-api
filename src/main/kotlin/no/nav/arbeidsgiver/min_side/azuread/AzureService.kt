48c57114c69f1e1f36a5ce200dad9d3f3a501055
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-10-08 12:43:11 +0200

    Fiks AzureService Cache

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/azuread/AzureService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/azuread/AzureService.kt
@@ -1,36 +1,29 @@
 package no.nav.arbeidsgiver.min_side.azuread
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.Expiry
-import no.nav.arbeidsgiver.min_side.config.Cache
+import no.nav.arbeidsgiver.min_side.getOrCompute
 import java.util.concurrent.TimeUnit
 
 
 class AzureService(
     private val azureClient: AzureClient,
 ) {
+    private val maxCachedEntries = 1000L
     private val cacheExpiryMarginSeconds = 60
 
-    private val cache = Cache(maximumSize = 1000) {
-        expireAfter(object : Expiry<String, AccessTokenEntry> {
+    private val cache: Cache<String, AccessTokenEntry> = Caffeine.newBuilder()
+        .expireAfter(object : Expiry<String, AccessTokenEntry> {
             override fun expireAfterCreate(key: String, response: AccessTokenEntry, currentTime: Long): Long {
                 return TimeUnit.SECONDS.toNanos(response.expiresInSeconds - cacheExpiryMarginSeconds)
             }
-
-            override fun expireAfterUpdate(
-                key: String,
-                value: AccessTokenEntry,
-                currentTime: Long,
-                currentDuration: Long
-            ): Long = currentDuration
-
-            override fun expireAfterRead(
-                key: String,
-                value: AccessTokenEntry,
-                currentTime: Long,
-                currentDuration: Long
-            ): Long = currentDuration
+            override fun expireAfterUpdate(key: String, value: AccessTokenEntry, currentTime: Long, currentDuration: Long): Long = currentDuration
+            override fun expireAfterRead(key: String, value: AccessTokenEntry, currentTime: Long, currentDuration: Long): Long = currentDuration
         })
-    }
+        .maximumSize(maxCachedEntries)
+        .build()
+
 
     suspend fun getAccessToken(scope: String): String {
         return cache.getOrCompute(scope) {
