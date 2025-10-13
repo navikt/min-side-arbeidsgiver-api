d04fbddf5ae370ad70c2279a42351950886ad511
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-10-13 13:52:51 +0200

    fix content type tokenX

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/services/tokenExchange/TokenExchangeClient.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/services/tokenExchange/TokenExchangeClient.kt
@@ -2,6 +2,7 @@ package no.nav.arbeidsgiver.min_side.services.tokenExchange
 
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.FormDataContent
 import io.ktor.http.*
 import no.nav.arbeidsgiver.min_side.defaultHttpClient
 
@@ -17,19 +18,23 @@ class TokenExchangeClientImpl(
     private val client = defaultHttpClient()
 
     override suspend fun exchange(subjectToken: String, audience: String): TokenXToken {
-        val response = client.post(properties.tokenEndpoint){
-            setBody(mapOf(
-                "grant_type" to listOf(TokenXProperties.GRANT_TYPE),
-                "client_assertion_type" to listOf(TokenXProperties.CLIENT_ASSERTION_TYPE),
-                "subject_token_type" to listOf(TokenXProperties.SUBJECT_TOKEN_TYPE),
-                "subject_token" to listOf(subjectToken),
-                "client_assertion" to listOf(clientAssertionTokenFactory.clientAssertion),
-                "audience" to listOf(audience),
-                "client_id" to listOf(properties.clientId)
-            ))
-            contentType(ContentType.Application.Json)
+        val response = client.post(properties.tokenEndpoint) {
+            contentType(ContentType.Application.FormUrlEncoded)
+            setBody(
+                FormDataContent(
+                    Parameters.build {
+                        append("grant_type", TokenXProperties.GRANT_TYPE)
+                        append("client_assertion_type", TokenXProperties.CLIENT_ASSERTION_TYPE)
+                        append("subject_token_type", TokenXProperties.SUBJECT_TOKEN_TYPE)
+                        append("subject_token", subjectToken)
+                        append("client_assertion", clientAssertionTokenFactory.clientAssertion)
+                        append("audience", audience)
+                        append("client_id", properties.clientId)
+                    }
+                )
+            )
         }
-        if (response.status != HttpStatusCode.OK){
+        if (response.status != HttpStatusCode.OK) {
             throw RuntimeException("Feil ved token exchange mot TokenX. Status=${response.status} message =${response.body<String>()}")
         }
 
