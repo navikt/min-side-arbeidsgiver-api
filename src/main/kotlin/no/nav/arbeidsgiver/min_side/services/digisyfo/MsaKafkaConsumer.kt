26fbf40c177d2fb36d6c894c16a151e1da2645cf
Ken Gullaksen <ken@glxn.net>
2026-05-05 11:13:30 +0200

    Revert "Team logging: separate team-logs stream for Kafka consumers and userInfo"

--- src/main/kotlin/no/nav/arbeidsgiver/min_side/services/digisyfo/MsaKafkaConsumer.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/min_side/services/digisyfo/MsaKafkaConsumer.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.*
 import no.nav.arbeidsgiver.min_side.infrastruktur.defaultJson
 import no.nav.arbeidsgiver.min_side.infrastruktur.isActiveAndNotTerminating
 import no.nav.arbeidsgiver.min_side.infrastruktur.logger
-import no.nav.arbeidsgiver.min_side.infrastruktur.teamLogger
 import no.nav.arbeidsgiver.min_side.services.tiltak.RefusjonStatusRepository
 import no.nav.arbeidsgiver.min_side.sykefravarstatistikk.MetadataVirksomhetKafkaKeyDto
 import no.nav.arbeidsgiver.min_side.sykefravarstatistikk.StatistikkategoriKafkaKeyDto
@@ -34,7 +33,6 @@ class MsaKafkaConsumer(
     private val config: KafkaConsumerConfig,
 ) {
     private val log = logger()
-    private val teamLog = teamLogger()
 
     private val properties = Properties().apply {
         put(ConsumerConfig.GROUP_ID_CONFIG, config.groupId)
@@ -67,13 +65,6 @@ class MsaKafkaConsumer(
 
                     if (records.any()) {
                         for (record in records) {
-                            record.also {
-                                teamLog.info(
-                                    "kafka record received groupId={} topic={} partition={} offset={} timestamp={} key={} value={}",
-                                    config.groupId, it.topic(), it.partition(), it.offset(),
-                                    it.timestamp(), it.key(), it.value()
-                                )
-                            }
                             try {
                                 processor.processRecord(record)
                             } catch (e: Exception) {
@@ -109,15 +100,6 @@ class MsaKafkaConsumer(
                     log.info("polled {} records {}", records.count(), config)
 
                     if (records.any()) {
-                        for (record in records) {
-                            record.also {
-                                teamLog.info(
-                                    "kafka record received groupId={} topic={} partition={} offset={} timestamp={} key={} value={}",
-                                    config.groupId, it.topic(), it.partition(), it.offset(),
-                                    it.timestamp(), it.key(), it.value()
-                                )
-                            }
-                        }
                         try {
                             processor.processRecords(records)
                         } catch (e: Exception) {
